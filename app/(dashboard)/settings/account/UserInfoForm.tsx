61606a0f5beef0993d92bac5e5e14774d0155f5b
Alex Mendes <alexanderhmendes@gmail.com>
2023-10-29 21:41:48 +0000

    fix: user info form values

--- app/(dashboard)/settings/account/UserInfoForm.tsx
+++ app/(dashboard)/settings/account/UserInfoForm.tsx
@@ -72,12 +72,7 @@ const UserInfoForm = ({
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<Inputs>({
-    defaultValues: {
-      name: user?.name || "",
-      email: user?.email,
-    },
-  })
+  } = useForm<Inputs>()
 
   const updateUser: SubmitHandler<Inputs> = async ({ name, email }) => {
     if (
@@ -154,7 +149,9 @@ const UserInfoForm = ({
                 id="name"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                 autoComplete="name"
-                {...register("name")}
+                {...register("name", {
+                  value: user?.name ?? "",
+                })}
               />
             </div>
             <div className="sm:grid sm:grid-cols-2 h-9 items-center">
@@ -169,7 +166,9 @@ const UserInfoForm = ({
                 id="email"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                 autoComplete="email"
-                {...register("email")}
+                {...register("email", {
+                  value: user?.email ?? "",
+                })}
               />
             </div>
 
