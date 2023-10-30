78a37e2f65c498b31ce5f417bfe5034d5948e1a2
Alex Mendes <alexanderhmendes@gmail.com>
2023-10-30 02:21:30 +0000

    feat: update related keys when getting a list of items

--- utils/api/queries.ts
+++ utils/api/queries.ts
@@ -1,23 +1,12 @@
-import { QueryKey, UseQueryOptions, UseQueryResult, useQuery } from "@tanstack/react-query";
+import { QueryKey, UseQueryOptions, UseQueryResult, useQuery, useQueryClient } from "@tanstack/react-query";
 import { apiClient } from "./client";
 import { getQueryKey } from "./query-keys";
 
-export const useApiListQuery = <
-  K extends keyof typeof apiClient,
-  TQueryFnData = Awaited<ReturnType<typeof apiClient[K]>>,
-  TError = unknown,
-  TData extends TQueryFnData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
->(
-  functionName: K,
-): UseQueryResult<TQueryFnData, TError> => {
-  const queryKey: TQueryKey = getQueryKey(functionName);
-
-  return useQuery<TQueryFnData, TError, TData, TQueryKey>({
-    queryKey,
-    queryFn: apiClient[functionName],
-  } as UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>);
-};
+type Options<K extends keyof typeof apiClient> = {
+  isSingle?: boolean,
+  id?: number,
+  relatedFunctionName?: K,
+}
 
 export const useApiQuery = <
   K extends keyof typeof apiClient,
@@ -27,20 +16,55 @@ export const useApiQuery = <
   TQueryKey extends QueryKey = QueryKey,
 >(
   functionName: K,
-  id?: number,
-) => {
+  { isSingle, id, relatedFunctionName }: Options<K> = {},
+): UseQueryResult<TQueryFnData, TError> => {
   const queryKey: TQueryKey = getQueryKey(functionName, id);
+  const queryClient = useQueryClient();
+  const enabled = !isSingle || typeof id !== "undefined";
+
+  const queryFn = isSingle
+  ? () => apiClient[functionName]({ id })
+  : apiClient[functionName]
+
+  const onSuccess = async (data: TQueryFnData) => {
+    if (!relatedFunctionName || !Array.isArray(data)) {
+      return
+    }
+
+    // Update the query cache for any related items.
+    data.forEach((item) => {
+      if (typeof item !== "object" || !item.id)  {
+        return
+      }
+
+      queryClient.setQueryData(
+        getQueryKey(relatedFunctionName, item.id),
+        item,
+      )
+    })
+  };
 
   return useQuery<TQueryFnData, TError, TData, TQueryKey>({
     queryKey,
-    queryFn: () => apiClient[functionName]({ id }),
-    enabled: typeof id !== "undefined",
+    queryFn,
+    enabled,
+    onSuccess,
   } as UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>);
 };
 
-export const useCurrentUser = () => useApiListQuery('getCurrentUser');
-export const useApiKeys = () => useApiListQuery('getApiKeys');
-export const useApiKey = (id?: number) => useApiQuery('getApiKey', id);
-export const useSilos = () => useApiListQuery('getSilos');
-export const useDeals = () => useApiListQuery('getDeals');
-export const useUsers = () => useApiListQuery('getUsers');
+export const useCurrentUser = () => useApiQuery('getCurrentUser')
+
+export const useApiKeys = () => useApiQuery('getApiKeys', {
+  relatedFunctionName: 'getApiKey',
+})
+
+export const useApiKey = (id?: number) => useApiQuery('getApiKey', {
+  id,
+  isSingle: true,
+})
+
+export const useSilos = () => useApiQuery('getSilos')
+
+export const useDeals = () => useApiQuery('getDeals')
+
+export const useUsers = () => useApiQuery('getUsers')
