900741c889c1e83d17afb099a64a4c568019cfc6
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-04 20:48:24 +0000

    feat: only show pending email change notice if sent in the past 24 hours (#18)
    * feat: add alert component
    
    * feat: only show pending email change notice if sent in the past 24 hours

--- app/(dashboard)/settings/account/UserInfoForm.tsx
+++ app/(dashboard)/settings/account/UserInfoForm.tsx
@@ -1,10 +1,6 @@
 "use client"
 
-import {
-  CheckIcon,
-  InformationCircleIcon,
-  PencilIcon,
-} from "@heroicons/react/20/solid"
+import { CheckIcon, PencilIcon } from "@heroicons/react/20/solid"
 import { useForm, SubmitHandler } from "react-hook-form"
 import { useEffect, useState } from "react"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
@@ -17,6 +13,7 @@ import { useCurrentUser } from "@/utils/api/queries"
 import { useMutation } from "@tanstack/react-query"
 import { apiClient } from "@/utils/api/client"
 import { useOptimisticUpdater } from "@/hooks/useOptimisticUpdater"
+import { Alert } from "@/components/Alert"
 
 // Track if the toast for email change has been shown already
 let alerted = false
@@ -171,22 +168,10 @@ const UserInfoForm = ({
               />
             </div>
 
-            <div className="p-4 rounded-md bg-blue-50">
-              <div className="flex">
-                <div className="flex-shrink-0">
-                  <InformationCircleIcon
-                    className="w-5 h-5 text-blue-400"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="flex-1 ml-3">
-                  <p className="text-sm text-blue-700">
-                    Updating your email requires confirmation through links sent
-                    to both the old and the new email addresses.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Alert type="info">
+              Updating your email requires confirmation through links sent to
+              both the old and the new email addresses.
+            </Alert>
           </form>
         ) : (
           <dl className="space-y-4">
@@ -208,23 +193,10 @@ const UserInfoForm = ({
             </div>
 
             {hasPendingEmailChange && (
-              <div className="p-4 rounded-md bg-blue-50">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <InformationCircleIcon
-                      className="w-5 h-5 text-blue-400"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <div className="flex-1 ml-3">
-                    <p className="text-sm text-blue-700">
-                      You have requested an email change. Please confirm it
-                      through the links sent to both the old and the new email
-                      addresses.
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <Alert dismissable type="info">
+                You have requested an email change. Please confirm it through
+                the links sent to both the old and the new email addresses.
+              </Alert>
             )}
           </dl>
         )}
--- app/(dashboard)/settings/account/page.tsx
+++ app/(dashboard)/settings/account/page.tsx
@@ -1,6 +1,9 @@
 import Heading from "@/components/Heading"
 import UserInfoForm from "./UserInfoForm"
 import { serverSupabase } from "@/utils/supabase/server-supabase"
+import { differenceInHours } from "date-fns"
+
+const PENDING_EMAIL_CHANGE_MAX_HOURS = 24
 
 const Page = async () => {
   const supabase = serverSupabase()
@@ -11,10 +14,18 @@ const Page = async () => {
 
   if (!authUser) throw "No user found."
 
+  // The pending email change will expire by default after 24 hours.
+  const hasRecentEmailChange = authUser.email_change_sent_at
+    ? differenceInHours(new Date(), new Date(authUser.email_change_sent_at)) <=
+      PENDING_EMAIL_CHANGE_MAX_HOURS
+    : false
+
+  const hasPendingEmailChange = !!authUser.new_email && hasRecentEmailChange
+
   return (
     <div className="space-y-4 sm:space-y-5">
       <Heading tag="h2">Account</Heading>
-      <UserInfoForm hasPendingEmailChange={!!authUser.new_email} />
+      <UserInfoForm hasPendingEmailChange={hasPendingEmailChange} />
     </div>
   )
 }
