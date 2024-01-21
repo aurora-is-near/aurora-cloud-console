83b9cc8f5e1cec42d3ab891cc75f914effc7ff4b
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-21 13:46:14 +0000

    fix: update deals api response

--- src/app/api/deals/route.ts
+++ src/app/api/deals/route.ts
@@ -1,13 +1,23 @@
 import { NextRequest } from "next/server"
 import { createApiEndpoint } from "@/utils/api"
-import { getDeals } from "@/utils/proxy-api/get-deals"
 import { ApiRequestContext } from "@/types/api"
+import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-client"
+import { assertValidSupabaseResult } from "@/utils/supabase"
 
 export const GET = createApiEndpoint(
   "getDeals",
   async (_req: NextRequest, ctx: ApiRequestContext) => {
+    const supabase = createAdminSupabaseClient()
+    const result = await supabase
+      .from("deals")
+      .select("id, created_at, name, team_id")
+      .order("created_at", { ascending: true })
+      .eq("team_id", ctx.team.id)
+
+    assertValidSupabaseResult(result)
+
     return {
-      deals: await getDeals(ctx.team),
+      deals: result.data,
     }
   },
 )
