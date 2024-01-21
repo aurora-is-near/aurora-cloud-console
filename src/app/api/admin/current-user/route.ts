be802006afd4f7fcab0937908d530bff1be76670
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-21 22:54:12 +0000

    fix: current user endpoint

--- src/app/api/admin/current-user/route.ts
+++ src/app/api/admin/current-user/route.ts
@@ -1,10 +1,8 @@
-import { User } from "@/types/types"
 import { apiRequestHandler } from "@/utils/api"
 import { ApiRequestContext } from "@/types/api"
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
 export const GET = apiRequestHandler(
   ["admin"],
-  async (_req: NextRequest, ctx: ApiRequestContext) =>
-    NextResponse.json<User>(ctx.user),
+  async (_req: NextRequest, ctx: ApiRequestContext) => ctx.user,
 )
