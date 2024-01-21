6000fc51c1dcce764869bc517da6165eb1a12415
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-21 13:50:28 +0000

    fix: update single dael api res

--- src/app/api/deals/[id]/route.ts
+++ src/app/api/deals/[id]/route.ts
@@ -1,18 +1,27 @@
 import { NextRequest } from "next/server"
 import { createApiEndpoint } from "@/utils/api"
 import { abort } from "../../../../utils/abort"
-import { getDealById } from "@/utils/proxy-api/get-deal-by-id"
 import { ApiRequestContext } from "@/types/api"
+import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-client"
+import { assertValidSupabaseResult } from "@/utils/supabase"
 
 export const GET = createApiEndpoint(
   "getDeal",
   async (_req: NextRequest, ctx: ApiRequestContext) => {
-    const deal = await getDealById(ctx.team, Number(ctx.params.id))
+    const supabase = createAdminSupabaseClient()
+    const result = await supabase
+      .from("deals")
+      .select("id, created_at, name, team_id")
+      .eq("id", Number(ctx.params.id))
+      .eq("team_id", ctx.team.id)
+      .maybeSingle()
 
-    if (!deal) {
+    assertValidSupabaseResult(result)
+
+    if (!result.data) {
       abort(404)
     }
 
-    return deal
+    return result.data
   },
 )
