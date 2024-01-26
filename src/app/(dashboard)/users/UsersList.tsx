7e6f8442393aef3a526956e453002ab31cc2da98
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-26 21:59:27 +0000

    fix: remove broken user export button, for now

--- src/app/(dashboard)/users/UsersList.tsx
+++ src/app/(dashboard)/users/UsersList.tsx
@@ -3,12 +3,10 @@
 import Button from "@/components/Button"
 import Heading from "@/components/Heading"
 import { Cog8ToothIcon } from "@heroicons/react/20/solid"
-import { ArrowDownCircleIcon } from "@heroicons/react/24/outline"
 import SearchInput from "./SearchInput"
 import UsersTable from "./UsersTable"
 import TableLoader from "@/components/TableLoader"
 import { useSearchParams } from "next/navigation"
-import { useMemo } from "react"
 import { useQuery } from "@tanstack/react-query"
 import { getQueryFnAndKey } from "@/utils/api/queries"
 
@@ -32,16 +30,6 @@ const UsersList = ({ title, dealId }: UsersListProps) => {
     }),
   )
 
-  const exportUrl = useMemo(() => {
-    const exportSearchParams = new URLSearchParams()
-
-    if (dealId) {
-      exportSearchParams.set("dealId", String(dealId))
-    }
-
-    return `/api/users/export?${exportSearchParams}`
-  }, [dealId])
-
   return (
     <div className="space-y-6">
       <header className="flex space-y-3 md:space-y-0 md:flex-row flex-col md:items-center md:justify-between lg:flex-col lg:space-y-3 xl:flex-row xl:space-y-0 lg:items-start xl:items-center xl:justify-between">
@@ -55,10 +43,6 @@ const UsersList = ({ title, dealId }: UsersListProps) => {
           <SearchInput search={search} />
 
           <div className="flex space-x-3">
-            <Button style="secondary" href={exportUrl} download="export.csv">
-              <ArrowDownCircleIcon className="w-5 h-5" />
-              <span>Export</span>
-            </Button>
             <Button>
               <Cog8ToothIcon className="w-5 h-5" />
               <span>Manage</span>
