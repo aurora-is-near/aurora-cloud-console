74d73d4b8deed188567fa4499d308098cae58d48
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-28 19:43:41 +0000

    feat: use react-dropdown-menu to let menu overflow container

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,7 +1,5 @@
-import { Fragment } from "react"
-import { Menu, Transition } from "@headlessui/react"
+import * as ReactDropdownMenu from "@radix-ui/react-dropdown-menu"
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline"
-import clsx from "clsx"
 
 type DropdownMenuProps = {
   menuItems: {
@@ -13,45 +11,35 @@ type DropdownMenuProps = {
 
 const DropdownMenu = ({ menuItems }: DropdownMenuProps) => {
   return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="flex items-center text-gray-600 rounded-full hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-green-600">
-          <span className="sr-only">Open options</span>
+    <ReactDropdownMenu.Root>
+      <ReactDropdownMenu.Trigger asChild>
+        <button
+          className="flex items-center text-gray-600 rounded-full hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-green-600"
+          aria-label="Open option"
+        >
           <EllipsisHorizontalIcon className="w-5 h-5" aria-hidden="true" />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 w-32 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-0.5">
-            {menuItems.map((item) => (
-              <Menu.Item key={item.text}>
-                {({ active }) => (
-                  <button
-                    onClick={item.onClick}
-                    className={clsx(
-                      "flex w-full items-center p-2.5 text-sm space-x-1",
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    )}
-                  >
-                    <item.Icon className="w-5 h-5" />
-                    <span>{item.text}</span>
-                  </button>
-                )}
-              </Menu.Item>
-            ))}
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+        </button>
+      </ReactDropdownMenu.Trigger>
+      <ReactDropdownMenu.Portal>
+        <ReactDropdownMenu.Content
+          className="absolute right-0 origin-top-right bg-white rounded-md shadow-lg"
+          sideOffset={5}
+        >
+          {menuItems.map((item) => (
+            <ReactDropdownMenu.Item key={item.text}>
+              <button
+                type="button"
+                onClick={item.onClick}
+                className="flex w-full items-center py-2.5 text-sm space-x-1 text-gray-700 px-4"
+              >
+                <item.Icon className="w-5 h-5" />
+                <span>{item.text}</span>
+              </button>
+            </ReactDropdownMenu.Item>
+          ))}
+        </ReactDropdownMenu.Content>
+      </ReactDropdownMenu.Portal>
+    </ReactDropdownMenu.Root>
   )
 }
 
