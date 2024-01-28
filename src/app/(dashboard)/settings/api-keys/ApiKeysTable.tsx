701d14de0d074505639bfdfc76909b8eee11bb50
Alex Mendes <alexander.h.mendes@gmail.com>
2024-01-28 22:55:12 +0000

    feat: use the dropdown list for the api keys table

--- src/app/(dashboard)/settings/api-keys/ApiKeysTable.tsx
+++ src/app/(dashboard)/settings/api-keys/ApiKeysTable.tsx
@@ -1,32 +1,29 @@
 "use client"
 
 import Table from "@/components/Table"
-import {
-  KeyIcon,
-  PencilSquareIcon,
-  TrashIcon,
-} from "@heroicons/react/24/outline"
+import { KeyIcon } from "@heroicons/react/24/outline"
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/20/solid"
 import AddApiKeyButton from "./AddApiKeyButton"
 import { relativeTime } from "human-date"
-import TableButton from "@/components/TableButton"
 import { useModals } from "@/hooks/useModals"
 import { Modals } from "@/utils/modals"
 import { useQueryState } from "next-usequerystate"
 import TableLoader from "../../../../components/TableLoader"
 import { useApiKeys } from "@/hooks/useApiKeys"
 import { NoDataCta } from "@/components/NoDataCta"
+import DropdownMenu from "@/components/DropdownMenu"
 
 export const ApiKeysTable = () => {
   const { data: apiKeys, isLoading } = useApiKeys()
   const [, setId] = useQueryState("id")
   const { openModal } = useModals()
 
-  const onEditApiKeyClick = (id: number) => {
+  const onEditClick = (id: number) => {
     setId(String(id))
     openModal(Modals.EditApiKey)
   }
 
-  const onRemoveApiKeyClick = (id: number) => {
+  const onDeleteClick = (id: number) => {
     setId(String(id))
     openModal(Modals.DeleteApiKey)
   }
@@ -58,25 +55,29 @@ export const ApiKeysTable = () => {
       {apiKeys?.map((apiKey) => (
         <Table.TR key={apiKey.id}>
           <Table.TD dark>{apiKey.key}</Table.TD>
-          <Table.TD dark>{apiKey.note}</Table.TD>
+          <Table.TD>{apiKey.note}</Table.TD>
           <Table.TD>{apiKey.scopes.join(", ")}</Table.TD>
           <Table.TD>
             {apiKey.last_used_at ? relativeTime(apiKey.last_used_at) : "Never"}
           </Table.TD>
           <Table.TD align="right">
-            <TableButton
-              srOnlyText="Edit API key"
-              Icon={PencilSquareIcon}
-              onClick={() => {
-                onEditApiKeyClick(apiKey.id)
-              }}
-            />
-            <TableButton
-              Icon={TrashIcon}
-              srOnlyText="Remove API key"
-              onClick={() => {
-                onRemoveApiKeyClick(apiKey.id)
-              }}
+            <DropdownMenu
+              menuItems={[
+                {
+                  Icon: PencilSquareIcon,
+                  text: "Edit",
+                  onClick: () => {
+                    onEditClick(apiKey.id)
+                  },
+                },
+                {
+                  Icon: TrashIcon,
+                  text: "Delete",
+                  onClick: () => {
+                    onDeleteClick(apiKey.id)
+                  },
+                },
+              ]}
             />
           </Table.TD>
         </Table.TR>
