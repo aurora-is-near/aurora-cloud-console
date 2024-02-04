cb6ac8951a9bfbfa86d63f002babd628497ce0c5
Alex Mendes <alexander.h.mendes@gmail.com>
2024-02-04 18:27:55 +0000

    refactor: remove unused getDealVarKey function

--- src/utils/proxy-api/get-deal-var-key.ts
+++
@@ -1,5 +0,0 @@
-export const getDealVarKey = (
-  customerId: number,
-  dealId: number,
-  suffix: "enabled" | "startTime" | "endTime",
-) => `deal::acc::customers::${customerId}::deals::${dealId}::${suffix}`
