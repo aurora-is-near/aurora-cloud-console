fa4c4a8e2a0d66ebe3d8a74205ce956c18fed972
Alex Mendes <alexander.h.mendes@gmail.com>
2024-02-08 23:06:02 +0000

    fix: remove problematic indexed db persist client

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,48 +1,16 @@
 "use client"
 
-import { QueryClient } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactNode } from "react"
-import { get, set, del } from "idb-keyval"
-import {
-  PersistedClient,
-  Persister,
-  PersistQueryClientProvider,
-} from "@tanstack/react-query-persist-client"
-import timestring from "timestring"
 
 type QueryProviderProps = {
   children: ReactNode
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: timestring("24h"),
-    },
-  },
-})
-
-const INDEXED_DB_KEY = "acc-queries"
-
-const persister: Persister = {
-  persistClient: async (client: PersistedClient) => {
-    await set(INDEXED_DB_KEY, client)
-  },
-  restoreClient: async () => {
-    return await get<PersistedClient>(INDEXED_DB_KEY)
-  },
-  removeClient: async () => {
-    await del(INDEXED_DB_KEY)
-  },
-}
+const queryClient = new QueryClient()
 
 export const QueryProvider = ({ children }: QueryProviderProps) => {
   return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{ persister }}
-    >
-      {children}
-    </PersistQueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
