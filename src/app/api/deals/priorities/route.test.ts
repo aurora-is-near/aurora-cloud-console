8448b8253c16ee6341ac3ebf5dc5cebce4705e12
Alex Mendes <alexander.h.mendes@gmail.com>
2024-03-02 12:46:14 +0000

    test: add a test for updating the priorities list

--- src/app/api/deals/priorities/route.test.ts
+++ src/app/api/deals/priorities/route.test.ts
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 import { NextRequest } from "next/server"
-import { GET } from "./route"
+import { GET, PUT } from "./route"
 import { createMockApiContext } from "../../../../../test-utils/create-mock-api-context"
 import {
   createSelect,
@@ -11,6 +11,7 @@ import {
 import { createMockDeals } from "../../../../../test-utils/factories/deal-factory"
 import { proxyApiClient } from "@/utils/proxy-api/client"
 import { createProxyApiObject } from "../../../../../test-utils/create-proxy-api-object"
+import { mockTeam } from "../../../../../test-utils/mock-team"
 
 jest.mock("../../../../utils/api", () => ({
   createApiEndpoint: jest.fn((_name, handler) => handler),
@@ -53,7 +54,10 @@ describe("Deal priorities route", () => {
       ;(proxyApiClient.view as jest.Mock).mockResolvedValue({
         responses: [
           {
-            objects: [createProxyApiObject("42"), createProxyApiObject("43")],
+            objects: [
+              createProxyApiObject("0042"),
+              createProxyApiObject("0043"),
+            ],
           },
           {
             objects: [
@@ -81,14 +85,81 @@ describe("Deal priorities route", () => {
           {
             dealId: 1,
             name: "Test Deal 1",
-            priority: "42",
+            priority: "0042",
           },
           {
             dealId: 2,
             name: "Test Deal 2",
-            priority: "43",
+            priority: "0043",
           },
         ],
+      })
+    })
+  })
+
+  describe("PUT", () => {
+    it("updates the priority list", async () => {
+      const req = new NextRequest(
+        new URL(`http://test.com/api/deals/priorities`),
+      )
+
+      const priorities = [
+        {
+          dealId: 1,
+          priority: "0042",
+        },
+        {
+          dealId: 2,
+          priority: "0043",
+        },
+      ]
+
+      const ctx = createMockApiContext({
+        body: {
+          priorities,
+        },
+      })
+
+      const result = await PUT(req, ctx)
+
+      expect(result).toEqual({
+        items: [
+          {
+            dealId: 1,
+            name: "Test Deal 1",
+            priority: "0042",
+          },
+          {
+            dealId: 2,
+            name: "Test Deal 2",
+            priority: "0043",
+          },
+        ],
+      })
+
+      expect(proxyApiClient.update).toHaveBeenCalledTimes(2)
+
+      priorities.forEach(({ dealId, priority }, index) => {
+        expect(proxyApiClient.update).toHaveBeenNthCalledWith(index + 1, [
+          {
+            op_type: "set",
+            template_key: "template::deal::acc::pointer",
+            var_key: `deal::acc::customers::${mockTeam.id}::dealByPrio::${priority}`,
+            var_type: "string",
+          },
+          {
+            op_type: "set_value",
+            string_value: dealId.toString(),
+            var_key: `deal::acc::customers::${mockTeam.id}::dealByPrio::${priority}`,
+            var_type: "string",
+          },
+          {
+            op_type: "insert",
+            set_element: priority,
+            var_key: `deal::acc::customers::${mockTeam.id}::dealPrios`,
+            var_type: "set",
+          },
+        ])
       })
     })
   })
