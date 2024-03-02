f00d07ea5af4c8b623170fdb051e9eae2d5f6675
Alex Mendes <alexander.h.mendes@gmail.com>
2024-03-02 19:21:31 +0000

    fix: sidebar styles on smaller screens

--- src/components/menu/SubMenuNav.tsx
+++ src/components/menu/SubMenuNav.tsx
@@ -269,7 +269,7 @@ const SubMenuNav = ({ isAdmin }: { isAdmin?: boolean }) => {
   const isSettingsRoute = route === "settings"
 
   return (
-    <aside className="inset-y-0 flex-col hidden p-6 overflow-y-auto bg-white border-r border-gray-200 w-72 lg:flex gap-y-7">
+    <aside className="inset-y-0 flex-col hidden p-6 overflow-y-auto bg-white border-r border-gray-200 w-72 lg:flex gap-y-7 min-w-[250px]">
       <Heading>{heading}</Heading>
 
       <nav className="flex flex-col flex-1 gap-y-4">
