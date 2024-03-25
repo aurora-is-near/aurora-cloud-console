1c2faec9f0a1bd4e8a7467cf6fc72c62e8e923a6
Alex Mendes <alexander.h.mendes@gmail.com>
2024-03-25 01:10:50 +0000

    fix: step number pos on mobile

--- src/app/(dashboard)/silos/[id]/oracle/OracleDeploymentStep.tsx
+++ src/app/(dashboard)/silos/[id]/oracle/OracleDeploymentStep.tsx
@@ -19,7 +19,7 @@ export const OracleDeploymentStep = ({
     <div className="flex flex-row relative">
       <div
         className={clsx(
-          "w-10 h-10 rounded-full flex items-center justify-center text-lg text-white font-medium mr-6 mt-6 border border-slate-50 outline outline-8 outline-slate-50 z-10",
+          "w-10 h-10 rounded-full flex items-center justify-center text-lg text-white font-medium mr-6 mt-4 md:mt-6 border border-slate-50 outline outline-8 outline-slate-50 z-10",
           isDisabled ? "bg-slate-400" : "bg-slate-900",
         )}
       >
@@ -31,7 +31,7 @@ export const OracleDeploymentStep = ({
       {connectingLine && (
         <div
           className={clsx(
-            "absolute top-16 left-[19px] h-full border border-1 border-slate-200",
+            "absolute top-10 md:top-16 left-[19px] h-full border border-1 border-slate-200",
             connectingLine === "dashed" && "border-dashed",
           )}
         />
