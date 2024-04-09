8107d8a2e3d428b2909c1cf6826b3024ecefd533
Alex Mendes <alexander.h.mendes@gmail.com>
2024-04-09 20:28:53 +0100

    fix: disable oracle enable button for now

--- src/app/(dashboard)/silos/[id]/oracle/OracleEnableButton.tsx
+++ src/app/(dashboard)/silos/[id]/oracle/OracleEnableButton.tsx
@@ -3,6 +3,9 @@ import { useOptimisticUpdater } from "@/hooks/useOptimisticUpdater"
 import { apiClient } from "@/utils/api/client"
 import { useMutation } from "@tanstack/react-query"
 
+// Remove this once we have a chance to finish the feature!
+const IS_TEMPORARILY_DISABLED = true
+
 export const OracleEnableButton = ({ siloId }: { siloId: number }) => {
   const getSiloOracleUpdater = useOptimisticUpdater("getSiloOracle")
 
@@ -16,7 +19,11 @@ export const OracleEnableButton = ({ siloId }: { siloId: number }) => {
   }
 
   return (
-    <Button className="w-full" disabled={isPending} onClick={onClick}>
+    <Button
+      className="w-full"
+      disabled={isPending || IS_TEMPORARILY_DISABLED}
+      onClick={onClick}
+    >
       Enable feature
     </Button>
   )
