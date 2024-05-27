f3752b768815bb1dddcca93b6a8f66502c1bd333
Alex Mendes <alexander.h.mendes@gmail.com>
2024-05-28 00:45:24 +0100

    fix: delete team redirect

--- src/app/dashboard/[teamKey]/admin/team/DeleteTeamButton.tsx
+++ src/app/dashboard/[teamKey]/admin/team/DeleteTeamButton.tsx
@@ -4,18 +4,15 @@ import { deleteTeam } from "@/actions/teams/delete-team"
 import { DeleteButton } from "@/components/DeleteButton"
 import { HOME_ROUTE } from "@/constants/routes"
 import { Team } from "@/types/types"
-import { useRouter } from "next/navigation"
 
 type DeleteTeamButtonProps = {
   team: Team
 }
 
 export const DeleteTeamButton = ({ team }: DeleteTeamButtonProps) => {
-  const router = useRouter()
-
   const onConfirmClick = async () => {
     await deleteTeam(team.id)
-    router.push(HOME_ROUTE)
+    window.location.pathname = HOME_ROUTE
   }
 
   return (
