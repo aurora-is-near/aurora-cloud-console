f239abd3103dd6f182664da5cd91a828d0900d9c
Alex Mendes <alex.mendes@aurora.dev>
2024-09-13 12:23:14 +0100

    Fix unused exports and dependencies (#76)
    * build: move eslint-config to dev dependencies
    
    * refactor: remove unused ToastSettingsProvider
    
    * refactor: remove unused export
    
    * refactor: update schema types

--- src/types/api-schemas.ts
+++ src/types/api-schemas.ts
@@ -1,14 +1,22 @@
 import { z } from "zod"
-import * as contract from "@/app/api/contract"
+import {
+  BridgeSchema as Bridge,
+  DealSchema as Deal,
+  ListSchema as List,
+  OracleSchema as Oracle,
+  SiloSchema as Silo,
+  SimpleListSchema as SimpleList,
+  TokenSchema as Token,
+  TransactionDataSchema as TransactionData,
+  WalletDetailsSchema as WalletDetails,
+} from "@/app/api/contract"
 
-export type DealSchema = z.infer<typeof contract.DealSchema>
-export type SiloSchema = z.infer<typeof contract.SiloSchema>
-export type TokenSchema = z.infer<typeof contract.TokenSchema>
-export type ListSchema = z.infer<typeof contract.ListSchema>
-export type SimpleListSchema = z.infer<typeof contract.SimpleListSchema>
-export type WalletDetailsSchema = z.infer<typeof contract.WalletDetailsSchema>
-export type TransactionDataSchema = z.infer<
-  typeof contract.TransactionDataSchema
->
-export type OracleSchema = z.infer<typeof contract.OracleSchema>
-export type BridgeSchema = z.infer<typeof contract.BridgeSchema>
+export type DealSchema = z.infer<typeof Deal>
+export type SiloSchema = z.infer<typeof Silo>
+export type TokenSchema = z.infer<typeof Token>
+export type ListSchema = z.infer<typeof List>
+export type SimpleListSchema = z.infer<typeof SimpleList>
+export type WalletDetailsSchema = z.infer<typeof WalletDetails>
+export type TransactionDataSchema = z.infer<typeof TransactionData>
+export type OracleSchema = z.infer<typeof Oracle>
+export type BridgeSchema = z.infer<typeof Bridge>
