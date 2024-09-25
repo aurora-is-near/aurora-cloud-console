e4578b65bfb5872cd30f1cb1011681507e7fbd03
hyper54n3 <thedalpra@gmail.com>
2024-09-25 11:48:30 +0100

    Update SignIn page layout WIP

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image"
 import { FullScreenPage } from "@/components/FullScreenPage"
+import AuroraLogo from "@/components/v2/AuroraLogo"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return <FullScreenPage>{children}</FullScreenPage>
+}
+
+export default Layout
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className="relative flex flex-col items-center justify-center h-screen w-full"
+      style={{
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundColor: "#111827",
+        backgroundImage: "url('/static/v2/images/authBg.svg')",
+      }}
+    >
+      <Image
+        width="100"
+        height="100"
+        src="/static/v2/images/authBg.svg"
+        alt="Aurora"
+      />
+      <div className="absolute top-10">
+        <AuroraLogo />
+      </div>
+      {children}
+    </div>
+  )
 }
 
 export default Layout
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,19 @@
 import LoginForm from "@/app/auth/login/LoginForm"
-import { AuthHeading } from "@/components/AuthHeading"
+import Heading from "@/components/Heading"
 
 const Page = () => {
   return (
-    <>
-      <AuthHeading className="my-10">Sign in to your account</AuthHeading>
-      <LoginForm />
-    </>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Heading className="mb-5 text-5xl">Sign in</Heading>
+      <span className="text-[16px] text-slate-400 mb-5">
+        Enter your email to receive a sign-in link.
+      </span>
+      <div className="flex flex-col divide-y divide-slate-700 items-center justify-center bg-slate-800 border border-slate-700 rounded-2xl">
+        <div className="p-8 w-96">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   )
 }
 
