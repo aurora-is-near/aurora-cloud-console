2674dc7220049c522b38fb0def2816ea3813dea6
alexandermendes <alexanderhmendes@gmail.com>
2024-09-28 22:47:31 +0100

    feat: add background to auth pages

--- src/components/AuthLayoutWrapper.tsx
+++ src/components/AuthLayoutWrapper.tsx
@@ -1,12 +1,24 @@
+import Image from "next/image"
 import AuroraLogo from "@/components/AuroraLogo"
 
 const AuthLayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen w-full bg-center bg-cover bg-[#111827] bg-[url('/static/v2/images/authBg.svg')]">
+    <div className="relative flex flex-col items-center justify-center h-screen w-full bg-slate-900">
+      <div className="absolute left-0 bottom-0 w-full h-1/3">
+        <Image
+          src="/static/v2/images/authBg.svg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="0 20%"
+          sizes="100vw"
+          alt=""
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-slate-900 to-transparent" />
+      </div>
       <div className="absolute top-10">
         <AuroraLogo />
       </div>
-      {children}
+      <div className="z-10">{children}</div>
     </div>
   )
 }
