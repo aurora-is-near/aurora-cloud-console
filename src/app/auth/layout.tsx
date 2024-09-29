0293efbcbdb707302be04995d203f8b0bbf1f0c3
alexandermendes <alexanderhmendes@gmail.com>
2024-09-29 08:03:24 +0100

    refactor: use tailwind classes for bg image

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,10 +6,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="relative flex flex-col items-center justify-center h-screen w-full bg-slate-900">
       <div className="absolute left-0 bottom-0 w-full h-1/3">
         <Image
+          className="object-cover object-[0_20%]"
           src="/static/v2/images/authBg.svg"
           layout="fill"
           objectFit="cover"
-          style={{ objectPosition: "0 20%" }}
           sizes="100vw"
           alt=""
         />
