936d3de792fcce28f8b38d5583df5d5423267e20
alexandermendes <alexanderhmendes@gmail.com>
2024-09-30 13:25:39 +0100

    feat: let admin users sign in directly

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -1,4 +1,4 @@
 import { ADMIN_EMAIL_DOMAIN } from "@/constants/auth"
 
-export const isAdminUser = (user: { email?: string }) =>
-  user.email?.split("@")[1] === ADMIN_EMAIL_DOMAIN
+export const isAdminUser = (email?: string) =>
+  email?.split("@")[1] === ADMIN_EMAIL_DOMAIN
