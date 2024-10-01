53f799ccf2687e2da0cef24cf48230ae3146daf8
alexandermendes <alexanderhmendes@gmail.com>
2024-10-01 22:54:18 +0100

    feat: add logout button to top menu always

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,3 +1,5 @@
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline"
+import clsx from "clsx"
 import { MenuItem } from "@/types/menu"
 import AuroraLogo from "@/components/AuroraLogo"
 import { MainMenuButton } from "./MenuButtons"
@@ -10,12 +12,24 @@ export const MainMenu = async ({ menuItems }: MainMenuProps) => {
   return (
     <div className="flex flex-row justify-between w-full bg-slate-900 px-4 py-4">
       <AuroraLogo />
-      <ul className="flex flex-row divide-x divide-slate-700 items-center">
+      <ul className="flex flex-row items-center">
         {menuItems.map((item) => (
           <li key={item.name} className="px-2">
             <MainMenuButton {...item} />
           </li>
         ))}
+        <li
+          className={clsx(
+            "pl-2",
+            menuItems.length && "border-l border-slate-700",
+          )}
+        >
+          <MainMenuButton
+            name="Logout"
+            href="/logout"
+            icon={<ArrowRightOnRectangleIcon />}
+          />
+        </li>
       </ul>
     </div>
   )
