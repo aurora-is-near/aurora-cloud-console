bf3332f523a1611bcce3c32638a0172e4f1ef588
hyper54n3 <thedalpra@gmail.com>
2024-10-02 20:18:55 +0100

    fix: wraps the RootLayout in a Suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next"
+import { Suspense } from "react"
 import { circular } from "@/styles/fonts/fonts"
 import "../styles/globals.css"
 import { Providers } from "@/app/Providers"
 import { ErrorModal } from "@/components/ErrorModal"
 import { MetaMaskNotInstalledModal } from "@/components/MetaMaskNotInstalledModal"
+import { FullScreenPage } from "@/components/FullScreenPage"
 
 export const metadata: Metadata = {
   title: "Aurora Cloud Console",
@@ -14,11 +16,19 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="h-full">
       <body className={`${circular.variable} font-sans h-full bg-gray-50`}>
-        <Providers>
-          {children}
-          <ErrorModal />
-          <MetaMaskNotInstalledModal />
-        </Providers>
+        <Suspense
+          fallback={
+            <FullScreenPage>
+              <div>Loading...</div>
+            </FullScreenPage>
+          }
+        >
+          <Providers>
+            {children}
+            <ErrorModal />
+            <MetaMaskNotInstalledModal />
+          </Providers>
+        </Suspense>
       </body>
     </html>
   )
