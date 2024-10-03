d54ac7436f27a4fd69640faca376bd4a0f115b30
alexandermendes <alexanderhmendes@gmail.com>
2024-10-03 23:54:03 +0100

    refactor: remove some more dead files

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,2 @@
 export { default as AuroraTriangle } from "./AuroraTriangle.svg"
-export { default as Borealis } from "./Borealis.svg"
 export { default as MetaMask } from "./MetaMask.svg"
--- src/components/menu/MenuItemLoader.tsx
+++
@@ -1,3 +0,0 @@
-import Loader from "@/components/Loader"
-
-export const MenuItemLoader = () => <Loader className="h-12 rounded-lg" />
