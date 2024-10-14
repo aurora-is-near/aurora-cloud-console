87515500d756ada4489af5c29e9fe08b961a0d1d
hyper54n3 <thedalpra@gmail.com>
2024-10-14 11:19:01 +0100

    fix: updates bridge hero image fix: updates bridge page details (text will be updated)

--- src/components/BridgePage/BridgePage.tsx
+++ src/components/BridgePage/BridgePage.tsx
@@ -3,6 +3,7 @@ import { Tabs } from "@/components/Tabs/Tabs"
 import Hero from "@/components/Hero/Hero"
 import { DashboardPage } from "@/components/DashboardPage"
 import { TabCard } from "@/components/TabCard/TabCard"
+import { RainbowBridge } from "../../../public/static/v2/images/icons"
 
 export const BridgePage = () => {
   return (
@@ -35,19 +36,21 @@ export const BridgePage = () => {
             content: (
               <TabCard
                 attribution={{
-                  text: "Powered by Munzen",
+                  text: "Powered by Rainbow Bridge",
+                  icon: <RainbowBridge />,
                 }}
               >
                 <div className="flex flex-col gap-2 text-slate-500">
                   <p>
-                    You can enable fiat onramp on your chain to let users
-                    purchase crypto assets with their credit card. Some assets
-                    are supported by default such as USDT, USDC, AURORA, NEAR,
-                    ETH.
+                    The forwarder leverages Near Protocol to automatically route
+                    assets to your chain. This means that any exchange that
+                    supports Near as a withdrawal method becomes compatible with
+                    your chain.
                   </p>
                   <p>
-                    To enable the purchase of your own asset, please get in
-                    touch with your account manager.
+                    The forwarder provides a unique Near address to each user
+                    that they can use to withdraw assets from the exchange to
+                    their account on your virtual chain.
                   </p>
                 </div>
               </TabCard>
