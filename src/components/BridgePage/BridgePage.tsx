b75429eaf02c96f83191b0effc3071905a26e677
alexandermendes <alexanderhmendes@gmail.com>
2024-10-16 08:08:28 +0100

    refactor: simplify BridgePage component

--- src/components/BridgePage/BridgePage.tsx
+++ src/components/BridgePage/BridgePage.tsx
@@ -3,40 +3,28 @@ import Image from "next/image"
 import { Tabs } from "@/components/Tabs/Tabs"
 import Hero from "@/components/Hero/Hero"
 import { DashboardPage } from "@/components/DashboardPage"
-import { TabCard } from "@/components/TabCard/TabCard"
 import BridgePageConfigurationTab from "@/components/BridgePage/BridgePageConfigurationTab"
+import { BridgePageAboutTab } from "@/components/BridgePage/BridgePageAboutTab"
 
 interface BridgePageProps {
-  teamKey?: string
+  teamKey: string
   siloId?: number
 }
 
-const aboutTab = {
-  title: "About",
-  content: (
-    <TabCard
-      attribution={{
-        text: "Powered by Munzen",
-      }}
-    >
-      <div className="flex flex-col gap-2 text-slate-500">
-        <p>
-          You can enable fiat onramp on your chain to let users purchase crypto
-          assets with their credit card. Some assets are supported by default
-          such as USDT, USDC, AURORA, NEAR, ETH.
-        </p>
-        <p>
-          To enable the purchase of your own asset, please get in touch with
-          your account manager.
-        </p>
-      </div>
-    </TabCard>
-  ),
-}
+export const BridgePage: React.FC<BridgePageProps> = ({ teamKey, siloId }) => {
+  const tabs = [{ title: "About", content: <BridgePageAboutTab /> }]
 
-const BridgePageLayout: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+  if (siloId) {
+    tabs.push({
+      title: "Configuration",
+      content: (
+        <BridgePageConfigurationTab
+          linkPrefix={`/dashboard/${teamKey}/silos/${siloId}/onramp`}
+        />
+      ),
+    })
+  }
+
   return (
     <DashboardPage>
       <Hero
@@ -59,46 +47,7 @@ const BridgePageLayout: React.FC<{ children: React.ReactNode }> = ({
           />
         }
       />
-      {children}
+      <Tabs tabs={tabs} />
     </DashboardPage>
-  )
-}
-
-const BridgePageContent: React.FC<{ teamKey: string; siloId: number }> = ({
-  teamKey,
-  siloId,
-}) => {
-  return (
-    <Tabs
-      tabs={[
-        aboutTab,
-        {
-          title: "Configuration",
-          content: (
-            <BridgePageConfigurationTab
-              linkPrefix={`/dashboard/${teamKey}/silos/${siloId}/onramp`}
-            />
-          ),
-        },
-      ]}
-    />
-  )
-}
-
-export const BridgePage: React.FC<BridgePageProps> = ({ teamKey, siloId }) => {
-  if (!teamKey || !siloId) {
-    return (
-      <BridgePageLayout>
-        <Tabs tabs={[aboutTab]} />
-      </BridgePageLayout>
-    )
-  }
-
-  const siloIdNumber = Number(siloId)
-
-  return (
-    <BridgePageLayout>
-      <BridgePageContent teamKey={teamKey} siloId={siloIdNumber} />
-    </BridgePageLayout>
   )
 }
