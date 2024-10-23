3d66a56c9ffd3824521b5f09ca55be1abcb7abb2
Alex Mendes <alex.mendes@aurora.dev>
2024-10-23 11:47:08 +0100

    fix: improve add asset button (#164)
    * fix: improve add asset button
    
    * fix: remove unused imports
    
    ---------
    
    Co-authored-by: hyper54n3 <thedalpra@gmail.com>

--- src/components/UniversalWidgetPage/DeployedTokensCard.tsx
+++ src/components/UniversalWidgetPage/DeployedTokensCard.tsx
@@ -3,13 +3,12 @@
 import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline"
 import clsx from "clsx"
 import { useState } from "react"
-import { PlusIcon } from "@heroicons/react/20/solid"
 import Card from "@/components/Card"
 import Loader from "@/components/Loader"
 import { Tag } from "@/components/Tag"
 import { useBridgeTokens } from "@/hooks/useBridgeTokens"
-import { Button } from "@/components/Button"
 import { TokensCard } from "@/components/UniversalWidgetPage/TokensCard"
+import { AddButton } from "@/components/AddButton"
 
 type DeployedTokensCardProps = {
   siloId: number
@@ -78,20 +77,13 @@ export const DeployedTokensCard = ({ siloId }: DeployedTokensCardProps) => {
               )
             })}
 
-            <Button
-              onClick={() => setIsAddingNewAsset(!isAddingNewAsset)}
-              variant="grey"
-              size="lg"
-            >
-              {isAddingNewAsset ? (
-                "Cancel"
-              ) : (
-                <div className="flex flex-row gap-2">
-                  <PlusIcon className="w-4 h-4" />
-                  <span>Add asset</span>
-                </div>
-              )}
-            </Button>
+            <AddButton
+              hideIcon={isAddingNewAsset}
+              text={isAddingNewAsset ? "Cancel" : "Add asset"}
+              onClick={() => {
+                setIsAddingNewAsset(!isAddingNewAsset)
+              }}
+            />
           </div>
         )}
       </div>
