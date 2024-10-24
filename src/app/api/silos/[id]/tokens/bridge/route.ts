22acd00e2727d036b9e853200290cbc875f88038
alexandermendes <alexanderhmendes@gmail.com>
2024-10-24 07:29:08 +0100

    fix: move bridgeSiloToken route

--- src/app/api/silos/[id]/tokens/bridge/route.ts
+++ src/app/api/silos/[id]/tokens/bridge/route.ts
@@ -7,7 +7,7 @@ import { getSiloTokenByAddress } from "@/actions/silo-tokens/get-silo-token-by-a
 import { createToken } from "@/actions/tokens/create-token"
 import { abort } from "../../../../../../utils/abort"
 
-const addToken = async (tokenId: number) => {
+const bridgeToken = async (tokenId: number) => {
   const updatedToken = await updateToken(tokenId, {
     bridge_deployment_status: "PENDING",
   })
@@ -15,7 +15,7 @@ const addToken = async (tokenId: number) => {
   return { status: updatedToken.bridge_deployment_status }
 }
 
-const addExistingToken = async (
+const bridgeExistingToken = async (
   siloId: number,
   tokenId: number,
 ): Promise<ApiResponseBody<"bridgeSiloToken">> => {
@@ -29,10 +29,10 @@ const addExistingToken = async (
     abort(400, "Token is already deployed")
   }
 
-  return addToken(token.id)
+  return bridgeToken(token.id)
 }
 
-const addCustomToken = async (
+const bridgeCustomToken = async (
   siloId: number,
   symbol: string,
   address: string,
@@ -44,7 +44,7 @@ const addCustomToken = async (
   }
 
   if (token) {
-    return addToken(token.id)
+    return bridgeToken(token.id)
   }
 
   const newToken = await createToken({
@@ -65,28 +65,25 @@ const addCustomToken = async (
   return { status: newToken.bridge_deployment_status }
 }
 
-export const POST = createApiEndpoint(
-  "bridgeSiloToken",
-  async (_req, ctx) => {
-    const silo = await getSilo(Number(ctx.params.id))
-    const badRequestMessage = "Must provide either tokenId or symbol/address"
+export const POST = createApiEndpoint("bridgeSiloToken", async (_req, ctx) => {
+  const silo = await getSilo(Number(ctx.params.id))
+  const badRequestMessage = "Must provide either tokenId or symbol/address"
 
-    if (!silo) {
-      abort(404)
-    }
+  if (!silo) {
+    abort(404)
+  }
 
-    if (ctx.body.tokenId && (ctx.body.symbol ?? ctx.body.address)) {
-      abort(400, badRequestMessage)
-    }
+  if (ctx.body.tokenId && (ctx.body.symbol ?? ctx.body.address)) {
+    abort(400, badRequestMessage)
+  }
 
-    if (ctx.body.tokenId) {
-      return addExistingToken(silo.id, ctx.body.tokenId)
-    }
+  if (ctx.body.tokenId) {
+    return bridgeExistingToken(silo.id, ctx.body.tokenId)
+  }
 
-    if (!ctx.body.symbol || !ctx.body.address) {
-      abort(400, badRequestMessage)
-    }
+  if (!ctx.body.symbol || !ctx.body.address) {
+    abort(400, badRequestMessage)
+  }
 
-    return addCustomToken(silo.id, ctx.body.symbol, ctx.body.address)
-  },
-)
+  return bridgeCustomToken(silo.id, ctx.body.symbol, ctx.body.address)
+})
