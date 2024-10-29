045c826c7b440fcd7a100fdff689b78c9276c390
alexandermendes <alexanderhmendes@gmail.com>
2024-10-29 20:12:36 +0000

    fix: can create deals check

--- src/utils/can-create-deal.ts
+++ src/utils/can-create-deal.ts
@@ -6,5 +6,5 @@ import { Deal, Silo } from "@/types/types"
 import { isDevNet } from "./is-dev-net"
 
 export const canCreateDeal = (silo: Silo, deals: Deal[]) =>
-  (isDevNet(silo) && deals.length >= MAX_DEALS_PER_SILO) ||
+  (isDevNet(silo) && deals.length < MAX_DEALS_PER_SILO) ||
   deals.length >= MAX_DEALS_PER_DEVNET_SILO
