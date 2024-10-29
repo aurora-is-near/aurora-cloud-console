fe11ca7cd3bb1c25abb46f67f69fa9e874b67eab
alexandermendes <alexanderhmendes@gmail.com>
2024-10-29 08:31:18 +0000

    feat: add new plans button

--- src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/GasAbstractionPlansTab.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/GasAbstractionPlansTab.tsx
@@ -1,4 +1,6 @@
+import { PlusIcon } from "@heroicons/react/20/solid"
 import { Deal, Silo } from "@/types/types"
+import { Button } from "@/components/Button"
 import DealItem from "./DealItem"
 
 type GasAbstractionPlansTabProps = {
@@ -12,13 +14,19 @@ export const GasAbstractionPlansTab = ({
   teamKey,
   deals,
 }: GasAbstractionPlansTabProps) => (
-  <div className="flex flex-col">
-    <div className="mt-2 mb-6">
-      <p className="text-sm text-slate-500">
+  <div className="flex flex-col w-full">
+    <div className="mt-2 mb-6 md:flex md:flex-row gap-x-10 w-full">
+      <p className="text-sm text-slate-500 max-w-2xl">
         Gas plans allow you to create custom logic about which transactions
         benefit from free transaction fees. You can configure it based on wallet
         addresses, contracts and add timeframe logic.
       </p>
+      <Button className="mt-5 md:mt-0 md:ml-auto">
+        <div className="flex w-full items-center gap-x-2">
+          <PlusIcon className="w-6 h-6" />
+          New plan
+        </div>
+      </Button>
     </div>
     <div className="flex flex-col gap-5">
       <ul className="grid gap-4 divide-gray-200">
