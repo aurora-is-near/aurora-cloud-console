8d1bde8367a65200df2ced11632397150d7aa812
alexandermendes <alexanderhmendes@gmail.com>
2024-10-30 08:14:01 +0000

    fix: update more weird options

--- src/app/dashboard/[teamKey]/silos/[id]/configuration/page.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/configuration/page.tsx
@@ -124,14 +124,14 @@ const Page = async ({
         description="Your virtual chain access restrictions and contract deployment support."
         items={[
           {
-            term: "Access controls",
-            description: "Enforced",
+            term: "Make transactions on chain",
+            description: "Deploy contracts",
             tooltip:
               "This whitelist contains the list of addresses allowed to interact with your chain.",
           },
           {
             term: "Contract deployment",
-            description: "Allowed",
+            description: "Allowed for everyone",
             tooltip:
               "This whitelist contains the list of addresses allowed to deploy contracts on your chain.",
           },
