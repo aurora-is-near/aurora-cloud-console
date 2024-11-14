da83f0d0d2a945521083b85aa3ba5c5e768c2c75
rfdlp <thedalpra@gmail.com>
2024-11-14 11:01:48 +0000

    feat: disable oracle in integrations when not deployed

--- src/app/dashboard/[teamKey]/silos/[id]/integrations/page.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/integrations/page.tsx
@@ -1,9 +1,20 @@
+import { getSiloOracle } from "@/actions/silo-oracle/get-silo-oracle"
 import { IntegrationsPage } from "@/components/IntegrationsPage/IntegrationsPage"
 
 const Page = async ({
   params: { id, teamKey },
 }: {
   params: { id: string; teamKey: string }
-}) => <IntegrationsPage teamKey={teamKey} siloId={Number(id)} />
+}) => {
+  const oracle = await getSiloOracle(Number(id))
+
+  return (
+    <IntegrationsPage
+      teamKey={teamKey}
+      siloId={Number(id)}
+      oracle={oracle ?? undefined}
+    />
+  )
+}
 
 export default Page
