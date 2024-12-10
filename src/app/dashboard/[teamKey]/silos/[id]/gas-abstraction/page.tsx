6c21980f81e4f36461dfc0ac9991d133416d1d4f
Vashchuk Maksim <vashchukmaksim@gmail.com>
2024-12-10 15:20:59 +0100

    fix: display gas collected tab only for aurora testnet

--- src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/page.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/page.tsx
@@ -1,10 +1,13 @@
+import { useMemo } from "react"
 import { notFound } from "next/navigation"
+
 import { getTeamSiloByKey } from "@/actions/team-silos/get-team-silo-by-key"
 import { getTeamDealsByKey } from "@/actions/team-deals/get-team-deals-by-key"
 import { Tabs } from "@/components/Tabs/Tabs"
 import { GasAbstractionPage } from "@/components/GasAbstractionPage"
 import { getTeamByKey } from "@/actions/teams/get-team-by-key"
 import { canCreateDeal } from "@/utils/can-create-deal"
+
 import { GasAbstractionPlansTab } from "./GasAbstractionPlansTab"
 import { GasAbstractionCollectedTab } from "./GasAbstractionCollectedTab"
 import { GasAbstractionAboutTab } from "./GasAbstractionAboutTab"
@@ -25,32 +28,40 @@ const Page = async ({
     notFound()
   }
 
+  const tabs = useMemo(() => {
+    const pageTabs = [
+      {
+        title: "About",
+        content: <GasAbstractionAboutTab />,
+      },
+      {
+        title: "Gas plans",
+        content: (
+          <GasAbstractionPlansTab
+            silo={silo}
+            teamKey={teamKey}
+            deals={deals}
+            isNewPlanButtonDisabled={!canCreateDeal(silo, deals)}
+          />
+        ),
+      },
+    ]
+
+    // For now we display "Gas collected" tab only for Aurora Testnet
+    if (silo.chain_id === "1313161555") {
+      pageTabs.push({
+        title: "Gas collected",
+        content: <GasAbstractionCollectedTab silo={silo} />,
+      })
+    }
+
+    return pageTabs
+  }, [silo])
+
   return (
     <>
       <GasAbstractionPage teamKey={teamKey} silo={silo}>
-        <Tabs
-          tabs={[
-            {
-              title: "About",
-              content: <GasAbstractionAboutTab />,
-            },
-            {
-              title: "Gas plans",
-              content: (
-                <GasAbstractionPlansTab
-                  silo={silo}
-                  teamKey={teamKey}
-                  deals={deals}
-                  isNewPlanButtonDisabled={!canCreateDeal(silo, deals)}
-                />
-              ),
-            },
-            {
-              title: "Gas collected",
-              content: <GasAbstractionCollectedTab silo={silo} />,
-            },
-          ]}
-        />
+        <Tabs tabs={tabs} />
       </GasAbstractionPage>
       <AddPlanModal team={team} />
     </>
