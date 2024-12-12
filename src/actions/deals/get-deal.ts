7ed098b0e8c0b213a445653b6a8a9c2749d338fb
rfdlp <thedalpra@gmail.com>
2024-12-12 12:55:48 +0000

    add team_id filter to deals query

--- src/actions/deals/get-deal.ts
+++ src/actions/deals/get-deal.ts
@@ -3,10 +3,18 @@
 import { Deal } from "@/types/types"
 import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-client"
 
-export const getDeal = async (id: number): Promise<Deal | null> => {
+export const getDeal = async (
+  id: number,
+  teamId: number,
+): Promise<Deal | null> => {
   const supabase = createAdminSupabaseClient()
 
-  const result = await supabase.from("deals").select("*").eq("id", id).single()
+  const result = await supabase
+    .from("deals")
+    .select("*")
+    .eq("id", id)
+    .eq("team_id", teamId)
+    .single()
 
   return result.data
 }
