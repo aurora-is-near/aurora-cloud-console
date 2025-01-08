1950966451e604329825ce9b3b740f2da7754797
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-08 15:59:59 +0100

    fix: return extra await

--- test-utils/regression/render.ts
+++ test-utils/regression/render.ts
@@ -4,7 +4,7 @@ import { wrapInHtmlTag } from "./html"
 import type { RenderOptions } from "./html"
 
 export const renderImage = async (html: string, options?: RenderOptions) => {
-  return await htmlToImage({
+  return htmlToImage({
     html: wrapInHtmlTag(html, options),
     waitUntil: "domcontentloaded",
     quality: 100,
