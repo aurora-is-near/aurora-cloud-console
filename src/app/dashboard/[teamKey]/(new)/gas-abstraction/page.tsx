a5760b9bd3757211c8624ccaefd66b08afeba91b
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-10 18:53:25 +0100

    fix: gas about tab added

--- src/app/dashboard/[teamKey]/(new)/gas-abstraction/page.tsx
+++ src/app/dashboard/[teamKey]/(new)/gas-abstraction/page.tsx
@@ -1,9 +1,22 @@
+import { Tabs } from "@/components/Tabs/Tabs"
 import { GasAbstractionPage } from "@/components/GasAbstractionPage"
+import { GasAbstractionAboutTab } from "@/components/GasAbstraction/AboutTab"
 
 const Page = async ({
   params: { teamKey },
 }: {
   params: { teamKey: string }
-}) => <GasAbstractionPage teamKey={teamKey} />
+}) => (
+  <GasAbstractionPage teamKey={teamKey}>
+    <Tabs
+      tabs={[
+        {
+          title: "About",
+          content: <GasAbstractionAboutTab />,
+        },
+      ]}
+    />
+  </GasAbstractionPage>
+)
 
 export default Page
