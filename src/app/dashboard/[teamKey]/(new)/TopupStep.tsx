917fd7df26ade44ad7a4a277b9a855e639ea7a1f
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-15 00:30:09 +0100

    fix: index prop is added to TopupStep

--- src/app/dashboard/[teamKey]/(new)/TopupStep.tsx
+++ src/app/dashboard/[teamKey]/(new)/TopupStep.tsx
@@ -6,16 +6,17 @@ import type { Team } from "@/types/types"
 import { StepCard } from "./StepCard"
 
 type Props = {
+  index: number
   team: Team
   state: "active" | "upcoming"
 }
 
-export const TopupStep = ({ team, state }: Props) => {
+export const TopupStep = ({ team, index, state }: Props) => {
   const topupLink = useStripePaymentLink(team)
 
   return (
     <StepCard
-      index={2}
+      index={index}
       state={state}
       title="Top up your transaction credits"
       description="Aurora does not charge a deployment fee or ongoing services fees for your Virtual Chain. You do however require transaction credits to settle transactions on NEAR Protocol. Your Virtual Chain can be set with custom gas fee to cover these transaction costs or to create an additional revenue stream for your project."
