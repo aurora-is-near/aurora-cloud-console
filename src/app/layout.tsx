cc3ec7b140f152990a3018098198243160b12202
rfdlp <thedalpra@gmail.com>
2025-01-16 12:05:38 +0000

    Install MixPanel browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Suspense } from "react"
+import Script from "next/script"
 import { circular } from "@/styles/fonts/fonts"
 import "../styles/globals.css"
 import { Providers } from "@/app/Providers"
@@ -13,6 +14,15 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="h-full">
+      <Script id="mixpanel-script" strategy="afterInteractive">
+        {`
+          mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
+            debug: true,
+            track_pageview: true,
+            persistence: 'localStorage'
+          });
+        `}
+      </Script>
       <body className={`${circular.variable} font-sans h-full bg-gray-50`}>
         <Providers>
           {children}
