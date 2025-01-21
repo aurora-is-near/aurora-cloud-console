9656c30163dd0c9ae50c1bf3f1c18de43ada9dc1
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-21 13:25:36 +0100

    fix: eslint ignore any

--- src/uikit/types.ts
+++ src/uikit/types.ts
@@ -8,6 +8,7 @@ export type HeroIcon = React.ComponentType<
 type _DeepPartialArray<T> = Array<DeepPartial<T>>
 type _DeepPartialObject<T> = { [P in keyof T]?: DeepPartial<T[P]> }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type DeepPartial<T> = T extends (...args: any[]) => any
   ? T
   : T extends Array<infer U>
