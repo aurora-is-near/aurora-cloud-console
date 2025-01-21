c2f7bc6f9d365a94d0c2d6d01af9ef49f3bd770f
alexandermendes <alexanderhmendes@gmail.com>
2025-01-21 00:15:07 +0000

    feat: add toast when feature flags updated

--- src/app/admin/feature-flags/FeatureFlagsForm.tsx
+++ src/app/admin/feature-flags/FeatureFlagsForm.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { sentenceCase } from "change-case"
+import toast from "react-hot-toast"
 import { HorizontalForm } from "@/components/HorizontalForm"
 import { featureFlags } from "@/feature-flags/browser"
 import { FeatureFlagId } from "@/types/feature-flags"
@@ -13,6 +14,8 @@ export const FeatureFlagsForm = () => {
     Object.entries(inputs).forEach(([id, value]) => {
       featureFlags.set(id as FeatureFlagId, value)
     })
+
+    toast.success("Feature flags updated")
   }
 
   return (
