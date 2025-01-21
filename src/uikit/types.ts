8f416136cd40214e456ee5b873fa3a642114b9a7
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-21 12:56:39 +0100

    chart tooltip and fixes

--- src/uikit/types.ts
+++ src/uikit/types.ts
@@ -4,3 +4,14 @@ export type HeroIcon = React.ComponentType<
     titleId?: string | undefined
   }
 >
+
+type _DeepPartialArray<T> = Array<DeepPartial<T>>
+type _DeepPartialObject<T> = { [P in keyof T]?: DeepPartial<T[P]> }
+
+export type DeepPartial<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+    ? _DeepPartialArray<U>
+    : T extends object
+      ? _DeepPartialObject<T>
+      : T | undefined
