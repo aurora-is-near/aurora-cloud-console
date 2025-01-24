13f2601ca5d62140f7b09dda537aec2cbf40d6e0
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-01-24 11:18:33 +0100

    fix: month to date block updated

--- src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/GasAbstractionConsumedTab.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/GasAbstractionConsumedTab.tsx
@@ -1,19 +1,18 @@
 "use client"
 
-import type { Silo } from "@/types/types"
+import type { Silo, Team } from "@/types/types"
 
 import { GasConsumedChart } from "./GasConsumedChart"
 import { GasConsumedMonthToDate } from "./GasConsumedMonthToDate"
 
 type Props = {
+  team: Team
   silo: Silo
 }
 
-export const GasAbstractionConsumedTab = ({ silo }: Props) => {
-  return (
-    <section className="w-full space-y-5">
-      <GasConsumedChart silo={silo} />
-      <GasConsumedMonthToDate silo={silo} />
-    </section>
-  )
-}
+export const GasAbstractionConsumedTab = ({ silo, team }: Props) => (
+  <section className="w-full space-y-5">
+    <GasConsumedChart silo={silo} />
+    <GasConsumedMonthToDate silo={silo} team={team} />
+  </section>
+)
