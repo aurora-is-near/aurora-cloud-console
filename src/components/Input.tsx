99bba1b3c12213552385223bcdf1ea8f2a1fd5fd
alexandermendes <alexanderhmendes@gmail.com>
2025-01-29 20:24:06 +0000

    refactor: remove unused file input styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -24,8 +24,6 @@ export const Input = <Inputs extends Record<string, unknown>>({
     disabled={disabled}
     className={clsx(
       "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6",
-      restProps.type === "file" &&
-        "file:mr-5 file:py-1 file:px-3 px-1.5 file:border-[1px] file:text-xs file:font-medium file:bg-slate-50 file:text-slate-700 hover:file:cursor-pointer",
       disabled ? "bg-gray-100 text-gray-500" : "text-gray-900",
       className,
     )}
