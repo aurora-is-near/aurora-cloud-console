d7f29ba8d0d2e331a584eef180d069cffcf5a266
Alex Mendes <alex.mendes@aurora.dev>
2025-02-11 21:27:12 +0000

    feat: add an admin section to download the onboarding report (#360)
    * feat: add mechanism to download the onboarding report
    
    * feat: improve csv report
    
    * fix: escape values for the csv report

---
+++ src/actions/reports/onboarding.ts
@@ -0,0 +1,62 @@
+"use server"
+
+import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-client"
+import {
+  assertNonNullSupabaseResult,
+  assertValidSupabaseResult,
+} from "@/utils/supabase"
+
+const getCsvSafeValue = (value?: string | number | null) => {
+  if (typeof value === "string") {
+    return value.replace(/"/g, '""')
+  }
+
+  return value
+}
+
+export const getOnboardingReport = async () => {
+  const supabase = createAdminSupabaseClient()
+  const result = await supabase.from("onboarding_form").select(
+    `
+        *,
+        teams (
+            name,
+            users_teams (
+                user:users (email)
+            )
+        )
+    `,
+  )
+
+  assertValidSupabaseResult(result)
+  assertNonNullSupabaseResult(result)
+
+  const csvData = result.data.map(({ teams: team, ...restRow }) => {
+    if (!team) {
+      return restRow
+    }
+
+    const emails = team.users_teams
+      .map((userTeam) => userTeam.user?.email)
+      .filter(Boolean)
+
+    return {
+      ...restRow,
+      team_name: team.name,
+      team_user_emails: emails,
+    }
+  })
+
+  return [
+    Object.keys(csvData[0]).join(","),
+    ...csvData.map((row) => {
+      return Object.values(row).map((value) => {
+        if (Array.isArray(value)) {
+          return `"[${value.map(getCsvSafeValue).join(", ")}]"`
+        }
+
+        return `"${getCsvSafeValue(value)}"`
+      })
+    }),
+  ].join("\n")
+}
