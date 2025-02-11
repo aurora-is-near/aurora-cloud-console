8f1cfdd03e0ffabbc46610e42ff44c6b54251a2a
rfdlp <thedalpra@gmail.com>
2025-02-11 17:38:52 +0000

    small QOL improvement on admin teams table

--- src/app/admin/teams/TeamsTable.tsx
+++ src/app/admin/teams/TeamsTable.tsx
@@ -10,6 +10,7 @@ import TableButton from "@/components/TableButton"
 import { OnboardingForm, Team } from "@/types/types"
 import { formatDate } from "@/utils/helpers"
 import { Input } from "@/components/Input"
+import { Card } from "@/uikit"
 import { DeleteTeamTableButton } from "./DeleteTeamTableButton"
 
 type Props = {
@@ -19,6 +20,7 @@ type Props = {
 
 const TeamsTable = ({ teams, onboardingForms }: Props) => {
   const [search, setSearch] = useState("")
+  const [expandedTeamId, setExpandedTeamId] = useState<number | null>(null)
 
   const filteredTeams = useMemo(() => {
     if (!search) {
@@ -51,18 +53,20 @@ const TeamsTable = ({ teams, onboardingForms }: Props) => {
         <Table.TH>ID</Table.TH>
         <Table.TH>Name</Table.TH>
         <Table.TH>Status</Table.TH>
-        <Table.TH>Requested base token</Table.TH>
-        <Table.TH>Telegram Handle</Table.TH>
-        <Table.TH align="center">Created at</Table.TH>
-        <Table.TH align="center">Updated at</Table.TH>
         <Table.TH hidden>Actions</Table.TH>
         {filteredTeams.map((team) => {
           const onboardingForm = onboardingForms.find(
             (form) => form.team_id === team.id,
           )
 
-          return (
-            <Table.TR key={team.id}>
+          return [
+            <Table.TR
+              key={team.id}
+              onClick={() =>
+                setExpandedTeamId(team.id === expandedTeamId ? null : team.id)
+              }
+              className={expandedTeamId === team.id ? "bg-slate-100" : ""}
+            >
               <Table.TD>{team.id}</Table.TD>
               <Table.TD>{team.name}</Table.TD>
               <Table.TD>
@@ -70,27 +74,6 @@ const TeamsTable = ({ teams, onboardingForms }: Props) => {
                   ? sentenceCase(team.onboarding_status)
                   : "-"}
               </Table.TD>
-              <Table.TD align="center">
-                {onboardingForm?.baseToken ?? "-"}
-              </Table.TD>
-              <Table.TD align="center">
-                {onboardingForm?.telegramHandle ? (
-                  <a
-                    href={`https://t.me/${onboardingForm.telegramHandle.replace("@", "")}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline"
-                  >
-                    {onboardingForm.telegramHandle.startsWith("@")
-                      ? onboardingForm.telegramHandle
-                      : `@${onboardingForm.telegramHandle}`}
-                  </a>
-                ) : (
-                  "-"
-                )}
-              </Table.TD>
-              <Table.TD align="center">{formatDate(team.created_at)}</Table.TD>
-              <Table.TD align="center">{formatDate(team.updated_at)}</Table.TD>
               <Table.TD align="right">
                 <div className="flex gap-x-3">
                   <TableButton
@@ -101,8 +84,47 @@ const TeamsTable = ({ teams, onboardingForms }: Props) => {
                   <DeleteTeamTableButton team={team} />
                 </div>
               </Table.TD>
-            </Table.TR>
-          )
+            </Table.TR>,
+            expandedTeamId === team.id && (
+              <Table.TR>
+                <Table.TD colSpan={100}>
+                  <div className="w-full grid grid-cols-3 gap-4 py-4">
+                    <Card className="flex flex-col md:p-2 gap-2">
+                      <strong>Telegram Handle</strong>
+                      <span>
+                        {onboardingForm?.telegramHandle ? (
+                          <a
+                            href={`https://t.me/${onboardingForm.telegramHandle.replace("@", "")}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="underline"
+                          >
+                            {onboardingForm.telegramHandle.startsWith("@")
+                              ? onboardingForm.telegramHandle
+                              : `@${onboardingForm.telegramHandle}`}
+                          </a>
+                        ) : (
+                          "-"
+                        )}
+                      </span>
+                    </Card>
+                    <Card className="flex flex-col md:p-2 gap-2">
+                      <strong>Requested base token</strong>
+                      <span>{onboardingForm?.baseToken ?? "-"}</span>
+                    </Card>
+                    <Card className="flex flex-col md:p-2 gap-2">
+                      <strong>Created at</strong>
+                      <span>{formatDate(team.created_at)}</span>
+                    </Card>
+                    <Card className="flex flex-col md:p-2 gap-2">
+                      <strong>Updated at</strong>
+                      <span>{formatDate(team.updated_at)}</span>
+                    </Card>
+                  </div>
+                </Table.TD>
+              </Table.TR>
+            ),
+          ].map((item) => item)
         })}
       </Table>
     </div>
