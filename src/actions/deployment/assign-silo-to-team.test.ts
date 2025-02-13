bfa01d905d939b5fb937a3ddda82cdf129e4123a
alexandermendes <alexanderhmendes@gmail.com>
2025-02-13 23:27:05 +0000

    test: add another test

--- src/actions/deployment/assign-silo-to-team.test.ts
+++ src/actions/deployment/assign-silo-to-team.test.ts
@@ -57,4 +57,33 @@ describe("assignSiloToTeam", () => {
       },
     ])
   })
+
+  it("does not set the base token details in the assigned silo for a custom token", async () => {
+    const mockSilo = createMockSilo()
+    const mockOnboardingForm = createMockOnboardingForm({
+      baseToken: "CUSTOM",
+      chainName: "Test Chain",
+    })
+
+    mockSupabaseClient
+      .from("silos")
+      .select.mockReturnValue(createSelect(mockSilo))
+
+    mockSupabaseClient
+      .from("silos")
+      .update.mockReturnValue(createInsertOrUpdate(mockSilo))
+
+    mockSupabaseClient
+      .from("onboarding_form")
+      .select.mockReturnValue(createSelect(mockOnboardingForm))
+
+    const result = await assignSiloToTeam(123)
+
+    expect(result).toEqual(mockSilo)
+
+    expect(mockSupabaseClient.from("silos").update).toHaveBeenCalledTimes(1)
+    expect(mockSupabaseClient.from("silos").update).toHaveBeenCalledWith({
+      name: "Test Chain",
+    })
+  })
 })
