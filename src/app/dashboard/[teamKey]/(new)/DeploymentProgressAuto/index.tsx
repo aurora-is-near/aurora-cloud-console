0096733df6aee6d1dbcf5f799b05f48fe8cdaad9
alexandermendes <alexanderhmendes@gmail.com>
2025-02-14 22:28:24 +0000

    refactor: use link button for open block explorer btn

--- src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.tsx
+++ src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.tsx
@@ -6,6 +6,7 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid"
 import { Button } from "@/components/Button"
 import type { Silo, Team } from "@/types/types"
 
+import { LinkButton } from "@/components/LinkButton"
 import { DeploymentSteps, ModalConfirmDeployment, Steps } from "./components"
 import { useSteps } from "./hooks"
 
@@ -85,18 +86,17 @@ export const DeploymentProgressAuto = ({
   // Deployment done
   return (
     <div className="flex gap-2">
-      {silo.explorer_url ? (
-        <Button
+      {!!silo.explorer_url && (
+        <LinkButton
+          isExternal
           size="lg"
           variant="primary"
-          onClick={() =>
-            silo.explorer_url && window.open(silo.explorer_url, "_blank")
-          }
+          href={silo.explorer_url}
         >
           Open Block Explorer
           <ArrowTopRightOnSquareIcon className="w-5 h-5" />
-        </Button>
-      ) : null}
+        </LinkButton>
+      )}
       <Button size="lg" variant="border">
         Customize Block Explorer
       </Button>
