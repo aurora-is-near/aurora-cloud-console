098673438a7922137a1b241541046b769b848b93
alexandermendes <alexanderhmendes@gmail.com>
2025-02-14 21:39:11 +0000

    feat: assign a silo when start deployment is clicked

--- src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.tsx
+++ src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid"
 
 import { Button } from "@/components/Button"
@@ -21,8 +20,6 @@ export const DeploymentProgressAuto = ({
   silo,
   isOnboardingFormSubmitted,
 }: Props) => {
-  const router = useRouter()
-  const [isDeploymentStarted] = useState(false)
   const [isConfirmDeploymentModalOpen, setIsConfirmDeploymentModalOpen] =
     useState(false)
 
@@ -36,7 +33,7 @@ export const DeploymentProgressAuto = ({
   })
 
   // Welcome
-  if (!silo && !isDeploymentStarted && !isOnboardingFormSubmitted) {
+  if (!silo && !isOnboardingFormSubmitted) {
     return (
       <Steps
         allSteps={allSteps}
@@ -49,7 +46,7 @@ export const DeploymentProgressAuto = ({
   }
 
   // Onboarding passed
-  if (!silo && !isDeploymentStarted && isOnboardingFormSubmitted) {
+  if (!silo) {
     return (
       <>
         <Steps
@@ -60,19 +57,16 @@ export const DeploymentProgressAuto = ({
           ]}
         />
         <ModalConfirmDeployment
+          team={team}
           isOpen={isConfirmDeploymentModalOpen}
-          onClickConfirm={() => null} // TODO: implement
           onClose={() => setIsConfirmDeploymentModalOpen(false)}
-          onClickEdit={() => {
-            router.push(`/dashboard/${team.team_key}/create-chain`)
-          }}
         />
       </>
     )
   }
 
-  // Deployment triggered
-  if (!silo && isDeploymentStarted) {
+  // Automatic deployment in progress
+  if (!silo.is_active) {
     return (
       <Steps
         allSteps={allSteps}
@@ -88,26 +82,23 @@ export const DeploymentProgressAuto = ({
   }
 
   // Deployment done
-  // TODO: && basedToken is assigned successfully
-  if (silo) {
-    return (
-      <div className="flex gap-2">
-        {silo.explorer_url ? (
-          <Button
-            size="lg"
-            variant="primary"
-            onClick={() =>
-              silo.explorer_url && window.open(silo.explorer_url, "_blank")
-            }
-          >
-            Open Block Explorer
-            <ArrowTopRightOnSquareIcon className="w-5 h-5" />
-          </Button>
-        ) : null}
-        <Button size="lg" variant="border">
-          Customize Block Explorer
+  return (
+    <div className="flex gap-2">
+      {silo.explorer_url ? (
+        <Button
+          size="lg"
+          variant="primary"
+          onClick={() =>
+            silo.explorer_url && window.open(silo.explorer_url, "_blank")
+          }
+        >
+          Open Block Explorer
+          <ArrowTopRightOnSquareIcon className="w-5 h-5" />
         </Button>
-      </div>
-    )
-  }
+      ) : null}
+      <Button size="lg" variant="border">
+        Customize Block Explorer
+      </Button>
+    </div>
+  )
 }
