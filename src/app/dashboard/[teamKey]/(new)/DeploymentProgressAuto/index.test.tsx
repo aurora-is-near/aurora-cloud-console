665adc688a7f866a06723fa348b82e6967f20421
alexandermendes <alexanderhmendes@gmail.com>
2025-02-17 21:19:01 +0000

    test: update tests

--- src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.test.tsx
+++ src/app/dashboard/[teamKey]/(new)/DeploymentProgressAuto/index.test.tsx
@@ -5,94 +5,104 @@ import { createWrapper } from "../../../../../../test-utils/create-wrapper"
 import { createMockSilo } from "../../../../../../test-utils/factories/silo-factory"
 
 describe("DeploymentProgressAuto", () => {
-  it("shows the expected welcome steps", async () => {
-    render(
-      <DeploymentProgressAuto
-        team={mockTeam}
-        silo={null}
-        isOnboardingFormSubmitted={false}
-        isDeploymentComplete={false}
-        setIsDeploymentComplete={() => {}}
-      />,
-      { wrapper: createWrapper() },
-    )
+  describe("welcome", () => {
+    it("shows the expected steps", async () => {
+      render(
+        <DeploymentProgressAuto
+          team={mockTeam}
+          silo={null}
+          isOnboardingFormSubmitted={false}
+          isDeploymentComplete={false}
+          setIsDeploymentComplete={() => {}}
+        />,
+        { wrapper: createWrapper() },
+      )
 
-    const steps = screen.getByTestId("deployment-steps")
+      const steps = screen.getByTestId("deployment-steps")
 
-    expect(
-      within(steps).getByText(/Configure your virtual chain/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Configure your virtual chain/i),
+      ).not.toBeNull()
 
-    expect(
-      within(steps).getByText(/Start automatic deployment/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Start automatic deployment/i),
+      ).not.toBeNull()
+    })
   })
 
-  it("shows the expected onboarding passed steps", async () => {
-    render(
-      <DeploymentProgressAuto
-        team={mockTeam}
-        silo={null}
-        isOnboardingFormSubmitted
-        isDeploymentComplete={false}
-        setIsDeploymentComplete={() => {}}
-      />,
-      { wrapper: createWrapper() },
-    )
+  describe("onboarding", () => {
+    it("shows the expected steps", async () => {
+      render(
+        <DeploymentProgressAuto
+          team={mockTeam}
+          silo={null}
+          isOnboardingFormSubmitted
+          isDeploymentComplete={false}
+          setIsDeploymentComplete={() => {}}
+        />,
+        { wrapper: createWrapper() },
+      )
 
-    const steps = screen.getByTestId("deployment-steps")
+      const steps = screen.getByTestId("deployment-steps")
 
-    expect(
-      within(steps).getByText(/Configure your virtual chain/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Configure your virtual chain/i),
+      ).not.toBeNull()
 
-    expect(
-      within(steps).getByText(/Start automatic deployment/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Start automatic deployment/i),
+      ).not.toBeNull()
+    })
   })
 
-  it("shows the expected deployment in progress steps", async () => {
-    render(
-      <DeploymentProgressAuto
-        team={mockTeam}
-        silo={createMockSilo({ is_active: true })}
-        isOnboardingFormSubmitted
-        isDeploymentComplete={false}
-        setIsDeploymentComplete={() => {}}
-      />,
-      { wrapper: createWrapper() },
-    )
+  describe("deployment in progress", () => {
+    it("shows the expected steps", async () => {
+      render(
+        <DeploymentProgressAuto
+          team={mockTeam}
+          silo={createMockSilo({ is_active: true })}
+          isOnboardingFormSubmitted
+          isDeploymentComplete={false}
+          setIsDeploymentComplete={() => {}}
+        />,
+        { wrapper: createWrapper() },
+      )
 
-    const steps = screen.getByTestId("deployment-steps")
+      const steps = screen.getByTestId("deployment-steps")
 
-    expect(
-      within(steps).getByText(/Initializing the Aurora engine/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Initializing the Aurora engine/i),
+      ).not.toBeNull()
 
-    expect(within(steps).getByText(/Setting up the base token/i)).not.toBeNull()
+      expect(
+        within(steps).getByText(/Setting up the base token/i),
+      ).not.toBeNull()
 
-    expect(
-      within(steps).getByText(/Starting your Block Explorer/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Starting your Block Explorer/i),
+      ).not.toBeNull()
 
-    expect(
-      within(steps).getByText(/Chain successfully deployed/i),
-    ).not.toBeNull()
+      expect(
+        within(steps).getByText(/Chain successfully deployed/i),
+      ).not.toBeNull()
+    })
   })
 
-  it("shows the expected content when deployment is complete", async () => {
-    render(
-      <DeploymentProgressAuto
-        team={mockTeam}
-        silo={createMockSilo({ is_active: true })}
-        isOnboardingFormSubmitted
-        isDeploymentComplete
-        setIsDeploymentComplete={() => {}}
-      />,
-      { wrapper: createWrapper() },
-    )
+  describe("deployment complete", () => {
+    it("shows the expected content", async () => {
+      render(
+        <DeploymentProgressAuto
+          team={mockTeam}
+          silo={createMockSilo({ is_active: true })}
+          isOnboardingFormSubmitted
+          isDeploymentComplete
+          setIsDeploymentComplete={() => {}}
+        />,
+        { wrapper: createWrapper() },
+      )
 
-    expect(screen.queryByTestId("deployment-steps")).toBeNull()
-    expect(screen.getByText(/Open Block Explorer/i)).not.toBeNull()
+      expect(screen.queryByTestId("deployment-steps")).toBeNull()
+      expect(screen.getByText(/Open Block Explorer/i)).not.toBeNull()
+    })
   })
 })
