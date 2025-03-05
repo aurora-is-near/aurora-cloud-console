60dc20f8064d9327f71e2e6d56c7e45ccb56feb0
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-03-05 14:00:33 +0100

    fix: knip

--- src/components/InfoList.tsx
+++ src/components/InfoList.tsx
@@ -22,46 +22,44 @@ const TextDescription = ({ children }: { children: string }) => {
   )
 }
 
-export const InfoList = ({ items, className }: InfoListProps) => {
-  return (
-    <dl className={clsx("divide-y divide-slate-200 flex-1", className)}>
-      {items.map(({ term, tooltip, description, Action }, index) => {
-        const isFirst = index === 0
-        const isLast = index === items.length - 1
+export const InfoList = ({ items, className }: InfoListProps) => (
+  <dl className={clsx("divide-y divide-slate-200 flex-1", className)}>
+    {items.map(({ term, tooltip, description, Action }, index) => {
+      const isFirst = index === 0
+      const isLast = index === items.length - 1
 
-        return (
-          <div
-            key={term}
-            className={clsx("grid grid-cols-5 items-center", {
-              "pb-2.5": isFirst,
-              "py-2 sm:py-2.5": !isFirst && !isLast,
-              "pt-2.5": isLast,
-            })}
-          >
-            <div className="col-span-2 flex items-center gap-x-1">
-              <dt className="text-sm font-medium text-slate-900">{term}</dt>
-              {!!tooltip && <InfoTooltip>{tooltip}</InfoTooltip>}
-            </div>
-            <div className="col-span-3 flex flex-row items-center justify-between gap-x-2.5">
-              <dd className="w-full flex-shrink-1">
-                {typeof description === "string" ? (
-                  <TextDescription>{description}</TextDescription>
-                ) : (
-                  description
-                )}
-              </dd>
-              {Action ? (
-                <Action
-                  term={term}
-                  description={
-                    typeof description === "string" ? description : undefined
-                  }
-                />
-              ) : null}
-            </div>
+      return (
+        <div
+          key={term}
+          className={clsx("grid grid-cols-5 items-center", {
+            "pb-2.5": isFirst,
+            "py-2 sm:py-2.5": !isFirst && !isLast,
+            "pt-2.5": isLast,
+          })}
+        >
+          <div className="col-span-2 flex items-center gap-x-1">
+            <dt className="text-sm font-medium text-slate-900">{term}</dt>
+            {!!tooltip && <InfoTooltip>{tooltip}</InfoTooltip>}
           </div>
-        )
-      })}
-    </dl>
-  )
-}
+          <div className="col-span-3 flex flex-row items-center justify-between gap-x-2.5">
+            <dd className="w-full flex-shrink-1">
+              {typeof description === "string" ? (
+                <TextDescription>{description}</TextDescription>
+              ) : (
+                description
+              )}
+            </dd>
+            {Action ? (
+              <Action
+                term={term}
+                description={
+                  typeof description === "string" ? description : undefined
+                }
+              />
+            ) : null}
+          </div>
+        </div>
+      )
+    })}
+  </dl>
+)
