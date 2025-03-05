123e49b80504ca429fe5033058e1c726b5eeab18
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-03-05 20:28:14 +0100

    fix: ff added

--- src/app/dashboard/[teamKey]/silos/[id]/configuration/EditPermissions.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/configuration/EditPermissions.tsx
@@ -6,6 +6,7 @@ import { PencilSquareIcon } from "@heroicons/react/24/solid"
 import { Button } from "@/uikit"
 import { Modals } from "@/utils/modals"
 import { useModals } from "@/hooks/useModals"
+import { useFeatureFlags } from "@/hooks/useFeatureFlags"
 import type { Silo, SiloWhitelistType } from "@/types/types"
 
 import { ConfigurationItemsCard } from "./ConfigurationItemsCard"
@@ -32,6 +33,9 @@ export const EditPermissions = ({ silo }: Props) => {
     null,
   )
 
+  const { flags } = useFeatureFlags()
+  const isWhitelistsEditingEnabled = flags.silo_whitelist_permissions
+
   const displayTxsWhitelistLabel = useDisplayValue(silo, "MAKE_TRANSACTION")
   const displayDeployWhitelistLabel = useDisplayValue(silo, "DEPLOY_CONTRACT")
 
@@ -48,32 +52,34 @@ export const EditPermissions = ({ silo }: Props) => {
             description: displayTxsWhitelistLabel,
             tooltip:
               "This whitelist contains the list of addresses allowed to interact with your chain.",
-            Action: () => (
-              <Button.Iconed
-                label="Edit"
-                icon={PencilSquareIcon}
-                onClick={() => {
-                  setCurrentModal("MAKE_TRANSACTION")
-                  openModal(Modals.EditSiloAddressPermissions)
-                }}
-              />
-            ),
+            Action: () =>
+              isWhitelistsEditingEnabled && (
+                <Button.Iconed
+                  label="Edit"
+                  icon={PencilSquareIcon}
+                  onClick={() => {
+                    setCurrentModal("MAKE_TRANSACTION")
+                    openModal(Modals.EditSiloAddressPermissions)
+                  }}
+                />
+              ),
           },
           {
             term: "Deploy contracts",
             description: displayDeployWhitelistLabel,
             tooltip:
               "This whitelist contains the list of addresses allowed to deploy contracts on your chain.",
-            Action: () => (
-              <Button.Iconed
-                label="Edit"
-                icon={PencilSquareIcon}
-                onClick={() => {
-                  setCurrentModal("DEPLOY_CONTRACT")
-                  openModal(Modals.EditSiloAddressPermissions)
-                }}
-              />
-            ),
+            Action: () =>
+              isWhitelistsEditingEnabled && (
+                <Button.Iconed
+                  label="Edit"
+                  icon={PencilSquareIcon}
+                  onClick={() => {
+                    setCurrentModal("DEPLOY_CONTRACT")
+                    openModal(Modals.EditSiloAddressPermissions)
+                  }}
+                />
+              ),
           },
         ]}
       />
