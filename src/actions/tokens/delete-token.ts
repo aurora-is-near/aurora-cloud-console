4f3f45ea37274a3e93e321c336c8d5dd06c5e864
alexandermendes <alexanderhmendes@gmail.com>
2025-03-07 07:58:32 +0000

    fix: remove deprecated tokens admin

--- src/actions/tokens/delete-token.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-client"
-
-export const deleteToken = async (id: number) => {
-  const supabase = createAdminSupabaseClient()
-
-  await supabase.from("tokens").delete().eq("id", id)
-}
