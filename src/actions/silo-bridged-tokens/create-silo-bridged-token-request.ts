2f463cf87247d205d87ed70d2a61c765f31c69b4
alexandermendes <alexanderhmendes@gmail.com>
2025-03-13 20:40:16 +0000

    fix: do not allow createSiloBridgedTokenRequest to be called with resolved_at

--- src/actions/silo-bridged-tokens/create-silo-bridged-token-request.ts
+++ src/actions/silo-bridged-tokens/create-silo-bridged-token-request.ts
@@ -5,7 +5,7 @@ import { BridgedTokenRequest } from "@/types/types"
 import { assertValidSupabaseResult } from "@/utils/supabase"
 
 export const createSiloBridgedTokenRequest = async (
-  inputs: Omit<BridgedTokenRequest, "id" | "created_at">,
+  inputs: Omit<BridgedTokenRequest, "id" | "created_at" | "resolved_at">,
 ): Promise<void> => {
   const supabase = createAdminSupabaseClient()
   const result = await supabase.from("bridged_token_requests").insert(inputs)
