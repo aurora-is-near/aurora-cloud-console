0f5cd21cb716ac3aa0d5225702fd12023a397f94
alexandermendes <alexanderhmendes@gmail.com>
2025-03-17 07:44:12 +0000

    feat: skip repairing silo if recently failed

--- src/actions/deployment/perform-deploy-token-transaction.ts
+++ src/actions/deployment/perform-deploy-token-transaction.ts
@@ -1,6 +1,5 @@
 "use server"
 
-import { JsonRpcProvider } from "ethers"
 import { performSiloConfigTransaction } from "@/actions/deployment/perform-silo-config-transaction"
 import {
   Silo,
@@ -8,9 +7,7 @@ import {
   SiloConfigTransactionStatus,
 } from "@/types/types"
 import { contractChangerApiClient } from "@/utils/contract-changer-api/contract-changer-api-client"
-import { checkTokenBySymbol } from "@/utils/check-token-contract"
 import { DefaultToken } from "@/types/default-tokens"
-import { DEFAULT_TOKENS } from "@/constants/default-tokens"
 
 const CONTRACT_CHANGER_SYMBOLS: Record<
   DefaultToken,
@@ -32,24 +29,20 @@ const SILO_CONFIG_TRANSACTION_OPERATIONS: Record<
   USDC: "DEPLOY_USDC",
 }
 
-const deployDefaultToken = async ({
-  provider,
+export const performDeployTokenTransaction = async ({
   silo,
   symbol,
   skipIfFailed,
 }: {
-  provider: JsonRpcProvider
   silo: Silo
   symbol: DefaultToken
   skipIfFailed?: boolean
 }): Promise<SiloConfigTransactionStatus> => {
-  if (await checkTokenBySymbol(provider, symbol)) {
-    return "SUCCESSFUL"
-  }
+  const operation = SILO_CONFIG_TRANSACTION_OPERATIONS[symbol]
 
   return performSiloConfigTransaction(
     silo,
-    SILO_CONFIG_TRANSACTION_OPERATIONS[symbol],
+    operation,
     async () =>
       contractChangerApiClient.mirrorErc20Token({
         siloEngineAccountId: silo.engine_account,
@@ -57,32 +50,4 @@ const deployDefaultToken = async ({
       }),
     { skipIfFailed },
   )
-}
-
-export const deployDefaultTokens = async (
-  silo: Silo,
-  { skipIfFailed }: { skipIfFailed?: boolean } = {},
-): Promise<SiloConfigTransactionStatus> => {
-  const provider = new JsonRpcProvider(silo.rpc_url)
-
-  const statuses = await Promise.all(
-    DEFAULT_TOKENS.map(async (symbol) =>
-      deployDefaultToken({
-        provider,
-        silo,
-        symbol,
-        skipIfFailed,
-      }),
-    ),
-  )
-
-  if (statuses.includes("PENDING")) {
-    return "PENDING"
-  }
-
-  if (statuses.includes("FAILED")) {
-    return "FAILED"
-  }
-
-  return "SUCCESSFUL"
 }
--- src/app/api/silos/repair/route.ts
+++ src/app/api/silos/repair/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest } from "next/server"
-import { JsonRpcProvider } from "ethers"
 import { createPrivateApiEndpoint } from "@/utils/api"
-import { checkDefaultTokens } from "@/utils/healthcheck"
 import { deployDefaultTokens } from "@/actions/deployment/deploy-default-tokens"
 import { getSilosToInspect } from "@/actions/silos/get-silos-to-inspect"
 import { Silo } from "@/types/types"
@@ -10,26 +8,18 @@ import { updateSilo } from "@/actions/silos/update-silo"
 const repairSilo = async (silo: Silo) => {
   // If the silo has never been inspected mark it as such and defer to the next
   // inspection. This is to help ensure we have a chance for the user to go
-  // through the onboarding process without this check kicking in at the wrong
-  // moment and taking over.
+  // through the onboarding process without this check interrupting and taking
+  // over before they have a chance to do so.
   if (!silo.inspected_at) {
     await updateSilo(silo.id, { inspected_at: new Date().toISOString() })
 
     return
   }
 
-  const provider = new JsonRpcProvider(silo.rpc_url)
-  const defaultTokensDeployed = await checkDefaultTokens(provider, silo)
+  const isWithin24Hours =
+    Date.now() - new Date(silo.inspected_at).getTime() < 24 * 60 * 60 * 1000
 
-  const needsDefaultTokensDeployed = Object.values(defaultTokensDeployed).some(
-    (isDeployed) => !isDeployed,
-  )
-
-  if (needsDefaultTokensDeployed) {
-    await deployDefaultTokens(silo)
-  }
-
-  await updateSilo(silo.id, { inspected_at: new Date().toISOString() })
+  await deployDefaultTokens(silo, { skipIfFailed: isWithin24Hours })
 }
 
 export const POST = createPrivateApiEndpoint(async (_req: NextRequest) => {
