c45f7a5667e7808c49d03a30a8f33cb10a9e2aee
alexandermendes <alexanderhmendes@gmail.com>
2025-03-17 18:26:45 +0000

    test: fix silo mock

--- test-utils/factories/silo-factory.ts
+++ test-utils/factories/silo-factory.ts
@@ -31,6 +31,7 @@ export const createMockSilo = (data?: Partial<Silo>): Silo => ({
   deleted_at: null,
   is_make_txs_public: false,
   is_deploy_contracts_public: false,
+  inspected_at: null,
   ...data,
 })
 
