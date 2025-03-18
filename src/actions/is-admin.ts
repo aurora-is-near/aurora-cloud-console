b6647ae886002a1e8a9d61ff932ba60d3de27771
rfdlp <thedalpra@gmail.com>
2025-03-18 13:57:40 +0000

    fix admin check to use new `getUser()`

--- src/actions/is-admin.ts
+++ src/actions/is-admin.ts
@@ -4,7 +4,7 @@ import { getAuthSession } from "@/actions/auth-session/get-auth-session"
 import { isAdminUser } from "@/utils/admin"
 
 export const isAdmin = async (): Promise<boolean> => {
-  const session = await getAuthSession()
+  const user = await getAuthSession()
 
-  return !!session?.user && isAdminUser(session.user.email)
+  return !!user && isAdminUser(user.email)
 }
