440949c1dce8c76b791a12182bc14ba6962a1184
rfdlp <thedalpra@gmail.com>
2025-03-18 13:21:20 +0000

    fix empty state missing required team prop

--- src/app/dashboard/[teamKey]/(new)/intents/page.tsx
+++ src/app/dashboard/[teamKey]/(new)/intents/page.tsx
@@ -1,5 +1,19 @@
+import { notFound } from "next/navigation"
+import { getTeamByKey } from "@/actions/teams/get-team-by-key"
 import { IntentsPage } from "@/components/IntentsPage/IntentsPage"
 
-const Page = () => <IntentsPage />
+const Page = async ({
+  params: { teamKey },
+}: {
+  params: { teamKey: string }
+}) => {
+  const team = await getTeamByKey(teamKey)
+
+  if (!team) {
+    notFound()
+  }
+
+  return <IntentsPage team={team} />
+}
 
 export default Page
