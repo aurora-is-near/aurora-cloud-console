0490909d79cf6e6ebfff8f9ec159d2ebb05d2d77
alexandermendes <alexanderhmendes@gmail.com>
2025-04-02 23:49:46 +0100

    Revert "Fixes abort to use the new type in `httpStatus`"
    This reverts commit 508ce67b2ed4c80277d13be6d912ba778b81ebab.

--- src/utils/abort.ts
+++ src/utils/abort.ts
@@ -1,7 +1,5 @@
 import httpStatus from "http-status"
 
-type HttpStatusCode = keyof typeof httpStatus
-
 type AbortOptions = {
   type?: string
   detail?: string
@@ -35,12 +33,12 @@ export const isAbortError = (error: unknown): error is AbortError =>
  * The error is intended to be picked up by the error handling middleware.
  */
 export function abort(
-  statusCode: HttpStatusCode,
+  statusCode: number,
   message?: string,
   abortOptions?: AbortOptions,
 ): never {
   const errorMessage = String(message ?? httpStatus[statusCode])
-  const error = new AbortError(Number(statusCode), errorMessage, abortOptions)
+  const error = new AbortError(statusCode, errorMessage, abortOptions)
 
   throw error
 }
