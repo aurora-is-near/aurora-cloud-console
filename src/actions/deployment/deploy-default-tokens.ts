b609a55759c8d62e72de3e8a765d1bcc45263a49
alexandermendes <alexanderhmendes@gmail.com>
2025-04-02 23:36:57 +0100

    feat: add ETH to default tokens

--- src/actions/deployment/deploy-default-tokens.ts
+++ src/actions/deployment/deploy-default-tokens.ts
@@ -22,6 +22,10 @@ const CONTRACT_CHANGER_SYMBOLS: Record<
   AURORA: "Aurora",
   USDt: "Usdt",
   USDC: "Usdc",
+  ETH: {
+    source_contract_id: "aurora",
+    nep141: "0x5a524251df27A25AC6b9964a93E1c23AD692688D",
+  },
 }
 
 const SILO_CONFIG_TRANSACTION_OPERATIONS: Record<
@@ -32,6 +36,7 @@ const SILO_CONFIG_TRANSACTION_OPERATIONS: Record<
   AURORA: "DEPLOY_AURORA",
   USDt: "DEPLOY_USDT",
   USDC: "DEPLOY_USDC",
+  ETH: "DEPLOY_ETH",
 }
 
 const checkContract = async ({
