6d4961423db986ead297b8d4ae08b10230043369
Vashchuk Maksim <vashchukmaksim@gmail.com>
2025-04-08 14:38:11 +0200

    fix: use ethers to format gas value

--- src/utils/decimals.ts
+++ src/utils/decimals.ts
@@ -5,10 +5,3 @@ export const decimalsToFloat = (value: number, decimals: number) => {
 export const floatToDecimals = (value: number, decimals: number) => {
   return value * 10 ** decimals
 }
-
-export const displayDecimalsWithoutScientificNotation = (
-  value: number,
-  decimals: number,
-) => {
-  return value.toFixed(decimals).replace(/(?<=\.\d*[1-9])0+$|\.0*$/, "")
-}
