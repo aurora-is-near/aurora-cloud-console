14f49489926e5de16dbc578125ced72e77f88493
alexandermendes <alexanderhmendes@gmail.com>
2025-04-09 22:40:25 +0100

    feat: add configure button to the bridge page

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,7 +2,7 @@
 
 import { paramCase } from "change-case"
 import { useSearchParams } from "next/navigation"
-import { ReactNode, useState } from "react"
+import { forwardRef, ReactNode, useImperativeHandle, useState } from "react"
 
 interface Tab {
   title: string
@@ -13,41 +13,60 @@ interface TabsProps {
   tabs: Tab[]
 }
 
-export const Tabs = ({ tabs }: TabsProps) => {
-  const searchParams = useSearchParams()
-  const [activeTab, setActiveTab] = useState<Tab | undefined>(() => {
-    const tabParam = searchParams.get("tab")
-    const tabKeys = tabs.map(({ title }) => paramCase(title))
-
-    const matchingTabIndex = tabKeys.findIndex((tabKey) => tabKey === tabParam)
-
-    return tabs[matchingTabIndex] ?? tabs[0]
-  })
-
-  return (
-    <div className="flex w-full flex-col gap-5">
-      <div className="flex self-start text-sm font-medium text-center">
-        <ul className="flex flex-wrap -mb-px">
-          {tabs.map((tab) => (
-            <li key={tab.title} className="me-0">
-              <button
-                type="button"
-                className={
-                  tab === activeTab
-                    ? "inline-block px-4 h-[48px] text-slate-900 border-b-2 border-green-600 rounded-t-lg active"
-                    : "inline-block px-4 h-[48px] border-b border-slate-200 rounded-t-lg hover:text-slate-600 text-slate-500 hover:border-gray-300"
-                }
-                onClick={() => {
-                  setActiveTab(tab)
-                }}
-              >
-                {tab.title}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex w-full">{activeTab?.content}</div>
-    </div>
-  )
+export type TabsFunctions = {
+  setActiveTabIndex: (index: number) => void
 }
+
+export const Tabs = forwardRef<TabsFunctions, TabsProps>(
+  ({ tabs }: TabsProps, ref) => {
+    const searchParams = useSearchParams()
+
+    const [activeTab, setActiveTab] = useState<Tab | undefined>(() => {
+      const tabParam = searchParams.get("tab")
+      const tabKeys = tabs.map(({ title }) => paramCase(title))
+
+      const matchingTabIndex = tabKeys.findIndex(
+        (tabKey) => tabKey === tabParam,
+      )
+
+      return tabs[matchingTabIndex] ?? tabs[0]
+    })
+
+    useImperativeHandle(ref, () => ({
+      setActiveTabIndex: (index: number) => {
+        const tab = tabs[index]
+
+        setActiveTab(tab)
+      },
+    }))
+
+    return (
+      <div className="flex w-full flex-col gap-5">
+        <div className="flex self-start text-sm font-medium text-center">
+          <ul className="flex flex-wrap -mb-px">
+            {tabs.map((tab) => (
+              <li key={tab.title} className="me-0">
+                <button
+                  type="button"
+                  className={
+                    tab === activeTab
+                      ? "inline-block px-4 h-[48px] text-slate-900 border-b-2 border-green-600 rounded-t-lg active"
+                      : "inline-block px-4 h-[48px] border-b border-slate-200 rounded-t-lg hover:text-slate-600 text-slate-500 hover:border-gray-300"
+                  }
+                  onClick={() => {
+                    setActiveTab(tab)
+                  }}
+                >
+                  {tab.title}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex w-full">{activeTab?.content}</div>
+      </div>
+    )
+  },
+)
+
+Tabs.displayName = "Tabs"
