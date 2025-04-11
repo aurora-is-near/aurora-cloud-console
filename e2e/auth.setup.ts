5ac871ecd4ed0c15496679b672b5d0638a1d6028
Maksim Vashchuk <vashchukmaksim@Maksims-MacBook-Pro.fritz.box>
2025-04-11 13:09:32 +0200

    fix: lint

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -1,5 +1,5 @@
-import { createClient } from "@supabase/supabase-js"
-import { chromium, FullConfig, Cookie } from "@playwright/test"
+import { createClient as supabaseCreateClient } from "@supabase/supabase-js"
+import { chromium } from "@playwright/test"
 
 import { PLAYWRIGHT_BASE_URL } from "../playwright.config"
 
@@ -7,29 +7,25 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 const TEST_USER_EMAIL = "maksim.vashchuk+e2e@aurora.dev"
-const TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD
+const { TEST_USER_PASSWORD } = process.env
 
-const getSupabaseProjectId = () => {
-  return SUPABASE_URL.split("//")[1].split(".")[0]
-}
-
-async function authSetup(config: FullConfig) {
+async function authSetup() {
   const browser = await chromium.launch()
   const context = await browser.newContext()
   const page = await context.newPage()
 
-  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+  const supabaseClient = supabaseCreateClient(SUPABASE_URL, SUPABASE_KEY)
 
   if (!TEST_USER_PASSWORD) {
     throw new Error("TEST_USER_PASSWORD is not set")
   }
 
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
     email: TEST_USER_EMAIL,
     password: TEST_USER_PASSWORD,
   })
 
-  if (error || !data.session) {
+  if (error ?? !data.session) {
     throw new Error(`Login failed: ${error?.message}`)
   }
 
@@ -38,16 +34,17 @@ async function authSetup(config: FullConfig) {
   await page.goto(`${PLAYWRIGHT_BASE_URL}/dashboard`)
 
   await page.evaluate(
-    async ([supabaseUrl, supabaseAnonKey, access_token, refresh_token]) => {
+    async ([supabaseUrl, supabaseAnonKey, accessToken, refreshToken]) => {
       const { createClient } = await import(
         // @ts-expect-error
         "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
       )
 
       const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
       await supabase.auth.setSession({
-        access_token,
-        refresh_token,
+        access_token: accessToken,
+        refresh_token: refreshToken,
       })
     },
     [SUPABASE_URL, SUPABASE_KEY, access_token, refresh_token],
