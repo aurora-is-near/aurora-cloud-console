a3d3fd12171d13f55754295b7c303f849cc78ab9
Maksim Vashchuk <vashchukmaksim@gmail.com>
2025-04-11 13:14:03 +0200

    fix: git creds

--- e2e/blockexploer.spec.ts
+++ e2e/blockexploer.spec.ts
@@ -5,5 +5,6 @@ import { PLAYWRIGHT_BASE_URL } from "../playwright.config"
 test("auth check", async ({ page, context }) => {
   await page.goto(`${PLAYWRIGHT_BASE_URL}/dashboard`)
   await expect(page).toHaveTitle(/Aurora Cloud Console/)
+  // for debugging
   await page.pause()
 })
