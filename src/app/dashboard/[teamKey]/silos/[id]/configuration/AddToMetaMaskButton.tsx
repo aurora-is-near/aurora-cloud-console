aa50ea195efd1e5ecf18bd5985ccc3c7df9bd474
alexandermendes <alexanderhmendes@gmail.com>
2025-04-18 15:36:20 +0100

    fix: use md button

--- src/app/dashboard/[teamKey]/silos/[id]/configuration/AddToMetaMaskButton.tsx
+++ src/app/dashboard/[teamKey]/silos/[id]/configuration/AddToMetaMaskButton.tsx
@@ -6,7 +6,7 @@ type AddToMetaMaskButtonProps = {
 }
 
 export const AddToMetaMaskButton = ({ onClick }: AddToMetaMaskButtonProps) => (
-  <Button onClick={onClick} size="sm" variant="border">
+  <Button onClick={onClick} variant="border">
     <PlusIcon className="w-4 h-4" />
     <span>Add to MetaMask</span>
   </Button>
