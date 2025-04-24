107f0b8d110afb9078f5331c550809227001c75d
rfdlp <thedalpra@gmail.com>
2025-04-24 13:32:25 +0100

    fix: update getCookie after upgrade

--- src/feature-flags/browser.ts
+++ src/feature-flags/browser.ts
@@ -5,6 +5,10 @@ import { FeatureFlagsClient } from "@/types/feature-flags"
 import { createFeatureFlagsClient } from "./utils"
 
 export const featureFlags: FeatureFlagsClient = createFeatureFlagsClient(
-  getCookie,
+  (id: string) => {
+    const value = getCookie(id)
+
+    return typeof value === "string" ? value : undefined
+  },
   setCookie,
 )
