85c7611f1970bbbf9ee061c38eb061358397058a
alexandermendes <alexanderhmendes@gmail.com>
2025-04-24 13:45:45 +0100

    fix: unassign deleted silos from teams

--- src/actions/silos/delete-silo.ts
+++ src/actions/silos/delete-silo.ts
@@ -5,6 +5,8 @@ import { createAdminSupabaseClient } from "@/supabase/create-admin-supabase-clie
 export const deleteSilo = async (id: number) => {
   const supabase = createAdminSupabaseClient()
 
+  await supabase.from("silos_teams").delete().eq("silo_id", id)
+
   await supabase
     .from("silos")
     .update({ deleted_at: new Date().toISOString() })
