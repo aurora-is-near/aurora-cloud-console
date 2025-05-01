cc48184c4360c960f6d45e4edf70f5baff300352
alexandermendes <alexanderhmendes@gmail.com>
2025-05-01 21:24:33 +0100

    fix: remove duplicate base container in MarketplaceGetStartedBanner

--- src/app/marketplace/MarketplaceGetStartedBanner.tsx
+++ src/app/marketplace/MarketplaceGetStartedBanner.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import clsx from "clsx"
 import { Heading } from "@/uikit/Typography/Heading"
-import { BaseContainer } from "@/components/BaseContainer"
 import { LinkButton } from "@/components/LinkButton"
 import { SIGNUP_ROUTE } from "@/constants/routes"
 
@@ -19,29 +18,27 @@ export const MarketplaceGetStartedBanner = ({
         className,
       )}
     >
-      <BaseContainer size="lg" className="h-full">
-        <div className="max-w-lg z-10 absolute top-0 left-0 h-full">
-          <div className="pl-24 flex flex-col justify-center h-full">
-            <Heading size={2} className="text-slate-50">
-              Deploy instantly with Aurora Cloud
-            </Heading>
-            <p className="text-slate-300 mt-2 xl:mt-4 text-sm xl:text-base">
-              Start with Aurora Cloud now and launch your virtual chain
-              instantly, with 1,000 free transactions ready to go.
-            </p>
-            <LinkButton href={SIGNUP_ROUTE} className="mt-3 xl:mt-5">
-              Get Started
-            </LinkButton>
-          </div>
+      <div className="max-w-lg z-10 absolute top-0 left-0 h-full">
+        <div className="pl-24 flex flex-col justify-center h-full">
+          <Heading size={2} className="text-slate-50">
+            Deploy instantly with Aurora Cloud
+          </Heading>
+          <p className="text-slate-300 mt-2 xl:mt-4 text-sm xl:text-base">
+            Start with Aurora Cloud now and launch your virtual chain instantly,
+            with 1,000 free transactions ready to go.
+          </p>
+          <LinkButton href={SIGNUP_ROUTE} className="mt-3 xl:mt-5">
+            Get Started
+          </LinkButton>
         </div>
-        <Image
-          src="/static/v2/images/marketplace/get-started-banner.png"
-          alt=""
-          width={2488}
-          height={676}
-          className="w-full h-auto object-cover"
-        />
-      </BaseContainer>
+      </div>
+      <Image
+        src="/static/v2/images/marketplace/get-started-banner.png"
+        alt=""
+        width={2488}
+        height={676}
+        className="w-full h-auto object-cover"
+      />
     </div>
   )
 }
