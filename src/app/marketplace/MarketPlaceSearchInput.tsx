1b62f8f76e776e1bffb60c1eb2647bd8966fe063
alexandermendes <alexanderhmendes@gmail.com>
2025-05-06 21:21:14 +0100

    feat: update search input active state

--- src/app/marketplace/MarketPlaceSearchInput.tsx
+++ src/app/marketplace/MarketPlaceSearchInput.tsx
@@ -2,8 +2,9 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 import clsx from "clsx"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { FormEvent, useRef } from "react"
+import { FormEvent, useRef, useState } from "react"
 
 type MarketPlaceSearchInputProps = {
   className?: string
@@ -14,6 +15,7 @@ export const MarketPlaceSearchInput = ({
 }: MarketPlaceSearchInputProps) => {
   const inputRef = useRef<HTMLInputElement>(null)
   const router = useRouter()
+  const [hasText, setHasText] = useState(false)
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -25,21 +27,30 @@ export const MarketPlaceSearchInput = ({
     }
   }
 
+  const handleChange = () => {
+    setHasText(!!inputRef.current?.value.trim())
+  }
+
   return (
     <form
       onSubmit={onSubmit}
       className={clsx(
-        "flex items-center justify-center w-full max-w-[270px] bg-slate-800 rounded-lg text-slate-300 overflow-hidden px-2.5",
+        "flex items-center justify-center w-full max-w-[270px] bg-slate-800 rounded-lg text-slate-300 overflow-hidden px-2.5 group focus-within:bg-slate-700",
         className,
       )}
     >
       <MagnifyingGlassIcon className="w-5 h-5 mr-2.5" />
       <input
+        required
         ref={inputRef}
         type="text"
         placeholder="Search apps"
-        className="w-full bg-transparent border-none p-0 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-300 focus:placeholder:text-slate-500"
+        className="w-full bg-transparent border-none p-0 py-2 text-slate-50 focus:outline-none focus:ring-0 placeholder:text-slate-300"
+        onChange={handleChange}
       />
+      {hasText && (
+        <Image src="/static/icons/return.svg" alt="" width={16} height={16} />
+      )}
     </form>
   )
 }
