41138b935385b87550a5d0513f66ff117748b464
alexandermendes <alexanderhmendes@gmail.com>
2025-05-06 20:59:53 +0100

    fix: add letter spacing to request an app

--- src/app/marketplace/MarketplaceRequestAppSection.tsx
+++ src/app/marketplace/MarketplaceRequestAppSection.tsx
@@ -9,7 +9,9 @@ export const MarketplaceRequestAppSection = ({
 }: MarketplaceRequestAppSectionProps) => {
   return (
     <section className={className}>
-      <h3 className="font-bold text-slate-900 text-lg">Request an app</h3>
+      <h3 className="font-bold text-slate-900 text-lg tracking-[-0.5px]">
+        Request an app
+      </h3>
       <p className="text-slate-500 text-sm mt-3">
         Can't find what you're looking for? Let us know.
       </p>
