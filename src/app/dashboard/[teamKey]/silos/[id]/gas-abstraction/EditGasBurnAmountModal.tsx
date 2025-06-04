c8037898a3b6f8cc77f452b358d6094ad18f2b19
Maksim Vashchuk <vashchukmaksim@gmail.com>
2025-06-04 17:23:23 +0200

    feat(burn-gas): edit gas burn value

---
+++ src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/EditGasBurnAmountModal.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import { z } from "zod"
+import toast from "react-hot-toast"
+import { useEffect, useId } from "react"
+import { useForm } from "react-hook-form"
+import { useQueryClient } from "@tanstack/react-query"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { CheckIcon } from "@heroicons/react/20/solid"
+import type { SubmitHandler } from "react-hook-form"
+
+import { logger } from "@/logger"
+import { Modals } from "@/utils/modals"
+import { useModals } from "@/hooks/useModals"
+import { Button } from "@/components/Button"
+import { InputField } from "@/components/InputField"
+import { updateSilo } from "@/actions/silos/update-silo"
+import SlideOver from "@/components/SlideOver"
+import { queryKeys } from "@/actions/query-keys"
+import type { Silo, Team } from "@/types/types"
+
+type FormData = {
+  gasBurn: number
+}
+
+type Props = {
+  silo: Silo
+  team: Team
+}
+
+export const EditGasBurnAmountModal = ({ team, silo }: Props) => {
+  const formId = useId()
+  const queryClient = useQueryClient()
+
+  const { closeModal, activeModal } = useModals()
+  const open = activeModal === Modals.EditGasBurn
+
+  const formSchema: z.ZodSchema<FormData> = z.object({
+    gasBurn: z.coerce
+      .number()
+      .min(0, { message: "Must be greater than 0" })
+      .max(100, { message: "Must be less than 100" }),
+  })
+
+  const {
+    watch,
+    register,
+    clearErrors,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    reValidateMode: "onSubmit",
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      gasBurn: silo.gas_burn_percent ?? 0,
+    },
+  })
+
+  const onSave: SubmitHandler<FormData> = async (values: FormData) => {
+    try {
+      await updateSilo(silo.id, { gas_burn_percent: values.gasBurn })
+      toast.success("Gas burn percentage updated.")
+      await queryClient
+        .invalidateQueries({
+          queryKey: queryKeys.getTeamSiloByKey(team.team_key, silo.id),
+        })
+        .then(closeModal)
+    } catch (e: unknown) {
+      logger.error(e)
+      toast.error("Gas burn percentage update failed.", {
+        position: "bottom-right",
+      })
+
+      return
+    }
+  }
+
+  const fieldValue = watch("gasBurn")
+
+  useEffect(() => {
+    clearErrors()
+  }, [fieldValue, clearErrors])
+
+  return (
+    <SlideOver open={open} close={closeModal} title="Gas burn">
+      <form
+        id={formId}
+        onSubmit={handleSubmit(onSave)}
+        className="flex flex-col gap-4"
+      >
+        <InputField
+          id="gasBurn"
+          name="gasBurn"
+          label="Set gas burn rate"
+          autoComplete="off"
+          errors={errors}
+          register={register}
+        />
+      </form>
+      <SlideOver.Actions>
+        <div className="flex items-center justify-between w-full">
+          <Button variant="secondary" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button type="submit" form={formId} loading={isSubmitting}>
+            <CheckIcon className="w-5 h-5" />
+            Update
+          </Button>
+        </div>
+      </SlideOver.Actions>
+    </SlideOver>
+  )
+}
---
+++ src/app/dashboard/[teamKey]/silos/[id]/gas-abstraction/GasBurn.test.tsx
@@ -0,0 +1,151 @@
+import { fireEvent, render, screen, within } from "@testing-library/react"
+
+import { getSilo } from "@/actions/silos/get-silo"
+import { getTeamByKey } from "@/actions/teams/get-team-by-key"
+import { updateSilo } from "@/actions/silos/update-silo"
+import { getLastDayOfMonth } from "@/utils/dates/get-last-day-of-month"
+import { getMonthsList } from "@/utils/dates/get-months-list"
+import { queryClient } from "@/providers/QueryProvider"
+
+import { createWrapper } from "../../../../../../../test-utils/create-wrapper"
+import { createMockSilo } from "../../../../../../../test-utils/factories/silo-factory"
+import { createMockTeam } from "../../../../../../../test-utils/factories/team-factory"
+
+import GasAbstractionPage from "./page"
+
+const team = createMockTeam()
+const silo = createMockSilo({
+  gas_burn_percent: 5,
+})
+
+jest.useFakeTimers()
+
+jest.mock("next/navigation", () => ({
+  useSearchParams: jest.fn(() => new URLSearchParams("tab=gas-collected")),
+  usePathname: () =>
+    `/dashboard/${team.team_key}/silos/${silo.id}/configuration`,
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    refresh: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+}))
+
+// mock server actions
+jest.mock("../../../../../../actions/teams/get-team-by-key")
+jest.mock("../../../../../../actions/silos/update-silo")
+jest.mock("../../../../../../actions/silos/get-silo")
+
+// mock collected gas chart data
+const monthsList = getMonthsList(silo.created_at)
+const startDate = monthsList[monthsList.length - 1].value
+
+describe("GasBurn", () => {
+  beforeEach(() => {
+    ;(getSilo as jest.Mock).mockResolvedValue(silo)
+    ;(getTeamByKey as jest.Mock).mockResolvedValue(team)
+
+    queryClient.setQueryData(["getSiloCollectedGasTotal", { id: silo.id }], {
+      count: 0.001,
+    })
+
+    queryClient.setQueryData(
+      [
+        "getSiloCollectedGas",
+        {
+          startDate,
+          endDate: getLastDayOfMonth(startDate),
+          id: silo.id,
+        },
+      ],
+      { items: [] },
+    )
+  })
+
+  afterEach(() => {
+    jest.clearAllTimers()
+  })
+
+  it("renders gas burn amount", async () => {
+    render(<GasAbstractionPage />, {
+      wrapper: createWrapper({
+        siloContext: {
+          silo: { ...silo, gas_burn_percent: 5 },
+        },
+        teamContext: { team },
+      }),
+    })
+
+    const card = screen.getByTestId("gas-mechanics-card")
+    const gasBurnItem = await within(card).findByLabelText(/Gas burn/)
+    const gasBurnValue =
+      await within(gasBurnItem).findByText(/5% of collected gas/)
+
+    expect(gasBurnValue).toBeInTheDocument()
+    const editBtn = within(gasBurnItem).queryByLabelText("Edit")
+    expect(editBtn).toBeInTheDocument()
+  })
+
+  it("enter wrong gas burn percentage", async () => {
+    render(<GasAbstractionPage />, {
+      wrapper: createWrapper({
+        siloContext: { silo },
+        teamContext: { team },
+      }),
+    })
+
+    const card = screen.getByTestId("gas-mechanics-card")
+    const gasBurnItem = await within(card).findByLabelText(/Gas burn/)
+
+    const editBtn = await within(gasBurnItem).findByLabelText("Edit")
+    fireEvent.click(editBtn)
+
+    const amountInput = await screen.findByLabelText(/Set gas burn rate/)
+    expect(amountInput).toBeInTheDocument()
+
+    fireEvent.change(amountInput, {
+      target: { value: "122" },
+    })
+
+    const updateBtn = await screen.findByRole("button", {
+      name: /Update/,
+    })
+
+    fireEvent.click(updateBtn)
+    expect(updateSilo).not.toHaveBeenCalled()
+  })
+
+  it("enter correct gas burn percentage", async () => {
+    render(<GasAbstractionPage />, {
+      wrapper: createWrapper({
+        siloContext: { silo },
+        teamContext: { team },
+      }),
+    })
+
+    const card = screen.getByTestId("gas-mechanics-card")
+    const gasBurnItem = await within(card).findByLabelText(/Gas burn/)
+
+    const editBtn = await within(gasBurnItem).findByLabelText("Edit")
+    fireEvent.click(editBtn)
+
+    const amountInput = await screen.findByLabelText(/Set gas burn rate/)
+
+    fireEvent.change(amountInput, {
+      target: { value: "10" },
+    })
+
+    const updateBtn = await screen.findByRole("button", {
+      name: /Update/,
+    })
+
+    fireEvent.click(updateBtn)
+    await screen.findByText(/Gas burn percentage updated/)
+
+    expect(updateSilo).toHaveBeenCalledTimes(1)
+    expect(updateSilo).toHaveBeenCalledWith(silo.id, {
+      gas_burn_percent: 10,
+    })
+  })
+})
