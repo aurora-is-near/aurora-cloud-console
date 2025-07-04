28a723ef0b3347da3043ea66e0bb4e0439e77e43
Diego Figueroa <diego.figueroa@aurora.dev>
2025-07-04 08:59:42 -0400

    chore: revert select <> input

--- src/app/admin/pauser/PauseForm.tsx
+++ src/app/admin/pauser/PauseForm.tsx
@@ -35,18 +35,16 @@ const PauseForm = () => {
             >
               Network
             </label>
-            <select
+            <input
+              type="text"
               id="networkId"
-              className="block w-full mt-2.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              className="block w-full mt-2.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              placeholder="near | ethereum"
               required
               {...register("networkId", {
-                required: "Please select a network",
+                required: "Please enter a networkId",
               })}
-            >
-              <option value="">Select network</option>
-              <option value="near">Near</option>
-              <option value="ethereum">Ethereum</option>
-            </select>
+            />
             {!!errors.networkId?.message && (
               <p className="mt-1.5 text-sm font-medium text-red-500">
                 {errors.networkId.message}
